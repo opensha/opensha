8c52352ebf9e2408cf3b41a33838f6b2e599cfa1
Kevin Milner <kmilner@usc.edu>
2017-10-20 15:01:50 -0700

    fixes case where last rupture on a patch has truncated transitions

--- src/main/java/org/opensha/sha/simulators/srf/RSQSimTransValidIden.java
+++ src/main/java/org/opensha/sha/simulators/srf/RSQSimTransValidIden.java
@@ -2,6 +2,8 @@ package org.opensha.sha.simulators.srf;
 
 import java.io.IOException;
 
+import org.opensha.commons.util.DataUtils;
+import org.opensha.commons.util.ExceptionUtils;
 import org.opensha.sha.simulators.EventRecord;
 import org.opensha.sha.simulators.RSQSimEvent;
 import org.opensha.sha.simulators.RSQSimEventRecord;
@@ -12,10 +14,14 @@ import com.google.common.base.Preconditions;
 
 public class RSQSimTransValidIden extends AbstractRuptureIdentifier {
 	
+	private RSQSimStateTransitionFileReader trans;
+	private double slipVel;
 	private double firstTrans;
 	private double lastTrans;
 	
-	public RSQSimTransValidIden(RSQSimStateTransitionFileReader trans) throws IOException {
+	public RSQSimTransValidIden(RSQSimStateTransitionFileReader trans, double slipVel) throws IOException {
+		this.trans = trans;
+		this.slipVel = slipVel;
 		firstTrans = trans.getFirstTransitionTime();
 		lastTrans = trans.getLastTransitionTime();
 	}
@@ -28,11 +34,32 @@ public class RSQSimTransValidIden extends AbstractRuptureIdentifier {
 			return false;
 		if (lastTrans - eventStart < 1000) {
 			// check ends
+			RSQSimEventSlipTimeFunc slipTime = null;
 			for (EventRecord rec : event) {
 				RSQSimEventRecord rRec = (RSQSimEventRecord)rec;
-				for (double nextTime : rRec.getNextSlipTimes()) {
-					if (Double.isFinite(nextTime) && nextTime > lastTrans)
-						return false;
+				double[] nextSlipTimes = rRec.getNextSlipTimes();
+				for (int i=0; i<nextSlipTimes.length; i++) {
+					double nextTime = nextSlipTimes[i];
+					if (!Double.isFinite(nextTime)) {
+						// check that it finished
+						if (slipTime == null) {
+							try {
+								slipTime = new RSQSimEventSlipTimeFunc(trans.getTransitions((RSQSimEvent)event), slipVel);
+							} catch (IOException e) {
+								throw ExceptionUtils.asRuntimeException(e);
+							} catch (IllegalStateException e) {
+								return false;
+							}
+							int patchID = rRec.getElementIDs()[i];
+							double slip = slipTime.getCumulativeEventSlip(patchID, slipTime.getEndTime());
+							double pDiff = DataUtils.getPercentDiff(slip, rRec.getElementSlips()[i]);
+							if (pDiff > 1)
+								return false;
+						}
+					} else {
+						if (nextTime > lastTrans)
+							return false;
+					}
 				}
 			}
 		}
