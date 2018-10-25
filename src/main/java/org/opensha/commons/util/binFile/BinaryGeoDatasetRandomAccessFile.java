f0bd33ffdf6004d08b40950ef3b33b02cd236488
Kevin Milner <kmilner@usc.edu>
2018-10-24 18:45:46 -0700

    added much faster static method to write dataset to file

--- src/main/java/org/opensha/commons/util/binFile/BinaryGeoDatasetRandomAccessFile.java
+++ src/main/java/org/opensha/commons/util/binFile/BinaryGeoDatasetRandomAccessFile.java
@@ -1,13 +1,20 @@
 package org.opensha.commons.util.binFile;
 
 import java.awt.geom.Point2D;
+import java.io.BufferedOutputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteOrder;
 
 import org.opensha.commons.data.xyz.ArbDiscrGeoDataSet;
 import org.opensha.commons.data.xyz.ArbDiscrXYZ_DataSet;
+import org.opensha.commons.data.xyz.GeoDataSet;
 import org.opensha.commons.geo.Location;
+
+import com.google.common.io.LittleEndianDataOutputStream;
 
 public class BinaryGeoDatasetRandomAccessFile extends BinaryXYZRandomAccessFile {
 
@@ -28,6 +35,26 @@ public class BinaryGeoDatasetRandomAccessFile extends BinaryXYZRandomAccessFile 
 			geo.set(new Location(pt.getY(), pt.getX()), val);
 		}
 		return geo;
+	}
+	
+	public static void writeGeoDataset(GeoDataSet data, ByteOrder byteOrder, File file) throws IOException {
+		BufferedOutputStream buf = new BufferedOutputStream(new FileOutputStream(file));
+		DataOutput dout;
+		if (byteOrder == ByteOrder.BIG_ENDIAN)
+			dout = new DataOutputStream(buf);
+		else
+			dout = new LittleEndianDataOutputStream(buf);
+		
+		dout.writeInt(data.size());
+		
+		for (int i=0; i<data.size(); i++) {
+			Location loc = data.getLocation(i);
+			dout.writeDouble(loc.getLongitude());
+			dout.writeDouble(loc.getLatitude());
+			dout.writeDouble(data.get(i));
+		}
+		
+		buf.close();
 	}
 
 }
