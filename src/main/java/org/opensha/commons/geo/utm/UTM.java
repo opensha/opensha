992466939e0075012cac40eabd96a9dec9c40576
Kevin Milner <kmilner@usc.edu>
2019-01-24 11:58:11 -0800

    can now force a specific zone/letter

--- src/main/java/org/opensha/commons/geo/utm/UTM.java
+++ src/main/java/org/opensha/commons/geo/utm/UTM.java
@@ -76,51 +76,64 @@ public class UTM
 	}
 
 	public UTM(WGS84 wgs) {
-		fromWGS84(wgs.getLatitude(), wgs.getLongitude());
+		fromWGS84(wgs.getLatitude(), wgs.getLongitude(), calcZone(wgs.getLongitude()), calcLetter(wgs.getLatitude()));
+	}
+	
+	public UTM(WGS84 wgs, int zone, char letter) {
+		fromWGS84(wgs.getLatitude(), wgs.getLongitude(), zone, letter);
+	}
+	
+	private static int calcZone(double longitude) {
+		return (int) Math.floor(longitude/6+31);
+	}
+	
+	private static char calcLetter(double latitude) {
+		if (latitude<-72) 
+			return 'C';
+		else if (latitude<-64) 
+			return 'D';
+		else if (latitude<-56)
+			return 'E';
+		else if (latitude<-48)
+			return 'F';
+		else if (latitude<-40)
+			return 'G';
+		else if (latitude<-32)
+			return 'H';
+		else if (latitude<-24)
+			return 'J';
+		else if (latitude<-16)
+			return 'K';
+		else if (latitude<-8) 
+			return 'L';
+		else if (latitude<0)
+			return 'M';
+		else if (latitude<8)  
+			return 'N';
+		else if (latitude<16) 
+			return 'P';
+		else if (latitude<24) 
+			return 'Q';
+		else if (latitude<32) 
+			return 'R';
+		else if (latitude<40) 
+			return 'S';
+		else if (latitude<48) 
+			return 'T';
+		else if (latitude<56) 
+			return 'U';
+		else if (latitude<64) 
+			return 'V';
+		else if (latitude<72) 
+			return 'W';
+		else
+			return 'X';
 	}
 
-	private void fromWGS84(double latitude, double longitude) {
-		zone= (int) Math.floor(longitude/6+31);
-		if (latitude<-72) 
-			letter='C';
-		else if (latitude<-64) 
-			letter='D';
-		else if (latitude<-56)
-			letter='E';
-		else if (latitude<-48)
-			letter='F';
-		else if (latitude<-40)
-			letter='G';
-		else if (latitude<-32)
-			letter='H';
-		else if (latitude<-24)
-			letter='J';
-		else if (latitude<-16)
-			letter='K';
-		else if (latitude<-8) 
-			letter='L';
-		else if (latitude<0)
-			letter='M';
-		else if (latitude<8)  
-			letter='N';
-		else if (latitude<16) 
-			letter='P';
-		else if (latitude<24) 
-			letter='Q';
-		else if (latitude<32) 
-			letter='R';
-		else if (latitude<40) 
-			letter='S';
-		else if (latitude<48) 
-			letter='T';
-		else if (latitude<56) 
-			letter='U';
-		else if (latitude<64) 
-			letter='V';
-		else if (latitude<72) 
-			letter='W';
-		else
-			letter='X';
+	private void fromWGS84(double latitude, double longitude, int zone, char letter) {
+		this.zone = zone;
+		this.letter = letter;
+		
 		easting=0.5*Math.log((1+Math.cos(latitude*Math.PI/180)*Math.sin(longitude*Math.PI/180-(6*zone-183)*Math.PI/180))/(1-Math.cos(latitude*Math.PI/180)*Math.sin(longitude*Math.PI/180-(6*zone-183)*Math.PI/180)))*0.9996*6399593.62/Math.pow((1+Math.pow(0.0820944379, 2)*Math.pow(Math.cos(latitude*Math.PI/180), 2)), 0.5)*(1+ Math.pow(0.0820944379,2)/2*Math.pow((0.5*Math.log((1+Math.cos(latitude*Math.PI/180)*Math.sin(longitude*Math.PI/180-(6*zone-183)*Math.PI/180))/(1-Math.cos(latitude*Math.PI/180)*Math.sin(longitude*Math.PI/180-(6*zone-183)*Math.PI/180)))),2)*Math.pow(Math.cos(latitude*Math.PI/180),2)/3)+500000;
 		easting=Math.round(easting*100)*0.01;
 		northing = (Math.atan(Math.tan(latitude*Math.PI/180)/Math.cos((longitude*Math.PI/180-(6*zone -183)*Math.PI/180)))-latitude*Math.PI/180)*0.9996*6399593.625/Math.sqrt(1+0.006739496742*Math.pow(Math.cos(latitude*Math.PI/180),2))*(1+0.006739496742/2*Math.pow(0.5*Math.log((1+Math.cos(latitude*Math.PI/180)*Math.sin((longitude*Math.PI/180-(6*zone -183)*Math.PI/180)))/(1-Math.cos(latitude*Math.PI/180)*Math.sin((longitude*Math.PI/180-(6*zone -183)*Math.PI/180)))),2)*Math.pow(Math.cos(latitude*Math.PI/180),2))+0.9996*6399593.625*(latitude*Math.PI/180-0.005054622556*(latitude*Math.PI/180+Math.sin(2*latitude*Math.PI/180)/2)+4.258201531e-05*(3*(latitude*Math.PI/180+Math.sin(2*latitude*Math.PI/180)/2)+Math.sin(2*latitude*Math.PI/180)*Math.pow(Math.cos(latitude*Math.PI/180),2))/4-1.674057895e-07*(5*(3*(latitude*Math.PI/180+Math.sin(2*latitude*Math.PI/180)/2)+Math.sin(2*latitude*Math.PI/180)*Math.pow(Math.cos(latitude*Math.PI/180),2))/4+Math.sin(2*latitude*Math.PI/180)*Math.pow(Math.cos(latitude*Math.PI/180),2)*Math.pow(Math.cos(latitude*Math.PI/180),2))/3);
