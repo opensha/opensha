abd843a9c3d24594a3b201bb8c9035f6762d2537
Kevin Milner <kmilner@usc.edu>
2020-04-13 10:24:05 -0700

    fixed bug with faster cartesian version

--- src/main/java/org/opensha/sha/simulators/iden/LinearRuptureIden.java
+++ src/main/java/org/opensha/sha/simulators/iden/LinearRuptureIden.java
@@ -2,6 +2,7 @@ package org.opensha.sha.simulators.iden;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 import org.opensha.commons.geo.Location;
@@ -33,6 +34,9 @@ public class LinearRuptureIden extends AbstractRuptureIdentifier {
 		this.fractional = fractional;
 		this.horzDistanceCache = horzDistanceCache;
 	}
+	
+	public static boolean D = false;
+	private static final boolean CARTESIAN = true;
 
 	@Override
 	public boolean isMatch(SimulatorEvent event) {
@@ -41,7 +45,7 @@ public class LinearRuptureIden extends AbstractRuptureIdentifier {
 			return true;
 		
 //		final boolean debug = event.getID() == 421261;
-		final boolean debug = false;
+		final boolean debug = false || D;
 		if (debug) System.out.println("Debugging event "+event.getID()+" with "+elems.size()+" elems");
 		
 		// find the 2 elements which are furthest away from each other
@@ -49,16 +53,35 @@ public class LinearRuptureIden extends AbstractRuptureIdentifier {
 		SimulatorElement furthest2 = null;
 		double maxDist = 0;
 		
+		HashSet<SimulatorElement> skipElems = new HashSet<>();
+		
 		for (int i=0; i<elems.size(); i++) {
 			SimulatorElement elem1 = elems.get(i);
+			if (skipElems.contains(elem1))
+				continue;
 			for (int j=i+1; j<elems.size(); j++) {
 				SimulatorElement elem2 = elems.get(j);
+				if (skipElems.contains(elem2))
+					continue;
 				IDPairing pair = elem1.getID() > elem2.getID() ? new IDPairing(elem2.getID(), elem1.getID()) : new IDPairing(elem1.getID(), elem2.getID());
 				Double dist = horzDistanceCache.get(pair);
 				if (dist == null) {
-					dist = LocationUtils.horzDistanceFast(elems.get(i).getCenterLocation(), elems.get(j).getCenterLocation());
+					Location l1 = elems.get(i).getCenterLocation();
+					Location l2 = elems.get(j).getCenterLocation();
+					if (CARTESIAN) {
+						// cartesian distance is fine here, just need to find furthest
+						double dLat = l1.getLatitude() - l2.getLatitude();
+						double dLon = l1.getLongitude() - l2.getLongitude();
+						dist = Math.sqrt(dLat*dLat + dLon*dLon);
+					} else {
+						dist = LocationUtils.horzDistanceFast(l1, l2);
+					}
+					
 					horzDistanceCache.put(pair, dist);
 				}
+				if (dist.floatValue() == 0f)
+					// horizontally identical to another element, don't bother check it later
+					skipElems.add(elem2);
 				if (dist > maxDist) {
 					furthest1 = elems.get(i);
 					furthest2 = elems.get(j);
@@ -67,6 +90,9 @@ public class LinearRuptureIden extends AbstractRuptureIdentifier {
 			}
 		}
 		
+		if (CARTESIAN)
+			// convert to km
+			maxDist = LocationUtils.horzDistanceFast(furthest1.getCenterLocation(), furthest2.getCenterLocation());
 		if (debug) System.out.println("\tMax dist: "+maxDist);
 		if (debug) System.out.println("\tElem 1 loc: "+furthest1.getCenterLocation());
 		if (debug) System.out.println("\tElem 2 loc: "+furthest2.getCenterLocation());
@@ -84,14 +110,27 @@ public class LinearRuptureIden extends AbstractRuptureIdentifier {
 		else
 			maxDeviation = this.maxDeviation;
 		
+		// create a conservatively sized rectangular region that, if it contains a given element, that element
+		// is surely no more than maxDeviation away
+//		double az = LocationUtils.azimuth(loc1, loc2)+90d;
+//		LocationList border = new LocationList();
+//		border.add(LocationUtils.location(loc1, new LocationVector(az, -0.6*maxDeviation, 0d)));
+//		border.add(LocationUtils.location(loc1, new LocationVector(az, 0.6*maxDeviation, 0d)));
+//		border.add(LocationUtils.location(loc2, new LocationVector(az, 0.6*maxDeviation, 0d)));
+//		border.add(LocationUtils.location(loc2, new LocationVector(az, -0.6*maxDeviation, 0d)));
+//		Region testReg = new Region(border, BorderType.GREAT_CIRCLE);
+		
 		for (SimulatorElement elem : elems) {
-			if (elem == furthest1 || elem == furthest2)
+			if (elem == furthest1 || elem == furthest2 || skipElems.contains(elem))
 				continue;
 			Location elemLoc = elem.getCenterLocation();
+//			if (testReg.contains(elemLoc))
+//				continue;
 			// strip out depth
 			elemLoc = new Location(elemLoc.getLatitude(), elemLoc.getLongitude());
 			double dist = Math.abs(LocationUtils.distanceToLineFast(loc1, loc2, elemLoc));
-			Preconditions.checkState(Double.isFinite(dist), "Bad dist: %s\n\tLoc1: %s\n\tLoc2: %s\n\tTest Loc: %s", dist, loc1, loc2, elemLoc);
+			Preconditions.checkState(Double.isFinite(dist), "Bad dist: %s\n\tLoc1: %s\n\tLoc2: %s\n\tTest Loc: %s",
+					dist, loc1, loc2, elemLoc);
 			if (debug) System.out.println("\t\tElem at "+elem.getCenterLocation()+": "+dist);
 			if (dist > maxDeviation)
 				return false;
