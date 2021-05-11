95a06e6f3f5dfd5393bd0ac0bc4134da902b97b6
Kevin Milner <kmilner@usc.edu>
2021-05-11 15:52:39 -0700

    updated prototype of CSV-based FSS/FSRS zip files

--- src/main/java/scratch/kevin/ucerf3/FSS_CSV_ZipTest.java
+++ src/main/java/scratch/kevin/ucerf3/FSS_CSV_ZipTest.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 import org.dom4j.DocumentException;
 import org.opensha.commons.data.CSVFile;
+import org.opensha.sha.magdist.IncrementalMagFreqDist;
+
+import com.google.common.base.Preconditions;
 
 import scratch.UCERF3.FaultSystemRupSet;
 import scratch.UCERF3.FaultSystemSolution;
@@ -17,13 +20,27 @@ public class FSS_CSV_ZipTest {
 	public static void main(String[] args) throws IOException, DocumentException {
 		FaultSystemSolution fss = FaultSystemIO.loadSol(new File(
 				"/home/kevin/OpenSHA/UCERF4/rup_sets/fm3_1_ucerf3.zip"));
+
+		CSVFile<String> rupMetaCSV = new CSVFile<>(true);
+		CSVFile<String> rupRatesCSV = new CSVFile<>(true);
+		CSVFile<String> rupSectsCSV = new CSVFile<>(false);
+		CSVFile<String> subSeismoMFDCSV = fss.getSubSeismoOnFaultMFD_List() == null ? null : new CSVFile<>(true);
+
+		rupMetaCSV.addLine("Rupture Index", "Magnitude", "Average Rake (degrees)", "Length (m)", "Area (m^2)");
+		rupRatesCSV.addLine("Rupture Index", "Annual Rate");
+		rupSectsCSV.addLine("Rupture Index", "# Subsections", "Subsection 1", "Subsection 2", "...", "Subsection N");
+
+		CSVFile<String> sectsMetaCSV = new CSVFile<>(true);
 		
-		CSVFile<String> metaCSV = new CSVFile<>(true);
-		CSVFile<String> sectsCSV = new CSVFile<>(false);
-		
-		metaCSV.addLine("Index", "Magnitude", "Average Rake (degrees)", "Length (m)",
-				"Area (m^2)", "Annual Rate");
-		sectsCSV.addLine("Index", "# Subsections", "Subsection 1", "Subsection 2", "...", "Subsection N");
+		sectsMetaCSV.addLine("Section Index", "Area (m^2)", "Target Slip Rate (m/yr)", "Slip Rate Std. Dev. (m/yr)");
+		if (subSeismoMFDCSV != null) {
+			List<String> line = new ArrayList<>();
+			line.add("Section Index");
+			IncrementalMagFreqDist xVals = fss.getSubSeismoOnFaultMFD_List().get(0);
+			for (int i=0; i<xVals.size(); i++)
+				line.add(xVals.getX(i)+"");
+			subSeismoMFDCSV.addLine(line);
+		}
 		
 		FaultSystemRupSet rupSet = fss.getRupSet();
 		for (int r=0; r<rupSet.getNumRuptures(); r++) {
@@ -33,18 +50,49 @@ public class FSS_CSV_ZipTest {
 			line.add(rupSet.getAveRakeForRup(r)+"");
 			line.add(rupSet.getLengthForRup(r)+"");
 			line.add(rupSet.getAreaForRup(r)+"");
-			line.add(fss.getRateForRup(r)+"");
-			metaCSV.addLine(line);
+//			line.add(fss.getRateForRup(r)+"");
+			rupMetaCSV.addLine(line);
 			List<Integer> sects = rupSet.getSectionsIndicesForRup(r);
 			line = new ArrayList<>();
-			line.add(sects.size()+"");
 			line.add(r+"");
+			line.add(sects.size()+"");
 			for (int s : sects)
 				line.add(s+"");
-			sectsCSV.addLine(line);
+			rupSectsCSV.addLine(line);
+			rupRatesCSV.addLine(r+"", fss.getRateForRup(r)+"");
 		}
-		metaCSV.writeToFile(new File("/tmp/fss_csv/test_meta.csv"));
-		sectsCSV.writeToFile(new File("/tmp/fss_csv/test_sects.csv"));
+		
+		boolean hasSlipSDs = rupSet.getSlipRateStdDevForAllSections() != null;
+		for (int s=0; s<rupSet.getNumSections(); s++) {
+			List<String> line = new ArrayList<>();
+			line.add(s+"");
+			line.add(rupSet.getAreaForSection(s)+"");
+			line.add(rupSet.getSlipRateForSection(s)+"");
+			if (hasSlipSDs)
+				line.add(rupSet.getSlipRateStdDevForSection(s)+"");
+			else
+				line.add("");
+			sectsMetaCSV.addLine(line);
+			if (subSeismoMFDCSV != null) {
+				line = new ArrayList<>();
+				line.add(s+"");
+				IncrementalMagFreqDist mfd = fss.getSubSeismoOnFaultMFD_List().get(s);
+				Preconditions.checkState(mfd.size() == subSeismoMFDCSV.getNumCols()-1,
+						"MFD for section %s has %s points, expected %s", s, mfd.size(), subSeismoMFDCSV.getNumCols()-1);
+				for (int i=0; i<mfd.size(); i++)
+					line.add(mfd.getY(i)+"");
+				subSeismoMFDCSV.addLine(line);
+			}
+		}
+		
+		File destDir = new File("/tmp/fss_csv/csvs_unzipped");
+		Preconditions.checkState(destDir.exists() || destDir.mkdirs());
+		rupMetaCSV.writeToFile(new File(destDir, "rupture_metadata.csv"));
+		rupSectsCSV.writeToFile(new File(destDir, "rupture_sects.csv"));
+		rupRatesCSV.writeToFile(new File(destDir, "rupture_rates.csv"));
+		sectsMetaCSV.writeToFile(new File(destDir, "section_metadata.csv"));
+		if (subSeismoMFDCSV != null)
+			subSeismoMFDCSV.writeToFile(new File(destDir, "section_sub_seismo_mfds.csv"));
 	}
 
 }
