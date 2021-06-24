2867be78e3d97f810ef5594f22a853357a670215
Kevin Milner <kmilner@usc.edu>
2021-06-23 18:05:09 -0700

    another merge attempt

--- src/main/java/org/opensha/commons/data/region/CaliforniaRegions.java
+++ src/main/java/org/opensha/commons/data/region/CaliforniaRegions.java
@@ -23,6 +23,7 @@ import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
