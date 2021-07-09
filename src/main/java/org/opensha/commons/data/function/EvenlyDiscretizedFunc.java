ebc841241f4e6fdbf2e58bbf90602c20185155fc
Kevin Milner <kmilner@usc.edu>
2021-07-09 16:53:15 -0700

    added default serialization for common function classes

--- src/main/java/org/opensha/commons/data/function/EvenlyDiscretizedFunc.java
+++ src/main/java/org/opensha/commons/data/function/EvenlyDiscretizedFunc.java
@@ -28,6 +28,7 @@ import org.apache.commons.io.IOUtils;
 import org.opensha.commons.exceptions.InvalidRangeException;
 
 import com.google.common.base.Preconditions;
+import com.google.gson.annotations.JsonAdapter;
 
 
 /**
@@ -56,7 +57,7 @@ import com.google.common.base.Preconditions;
  * @author Steven W. Rock
  * @version 1.0
  */
-
+@JsonAdapter(EvenlyDiscretizedFunc.Adapter.class)
 public class EvenlyDiscretizedFunc extends AbstractDiscretizedFunc{
 
 	private static final long serialVersionUID = 0xC4E0D3D;
@@ -602,5 +603,17 @@ public class EvenlyDiscretizedFunc extends AbstractDiscretizedFunc{
 //			Preconditions.checkState(ynew == yold);
 //		}
 //	}
+	
+	public static class Adapter extends DiscretizedFunc.AbstractAdapter<EvenlyDiscretizedFunc> {
+
+		@Override
+		protected EvenlyDiscretizedFunc instance(Double minX, Double maxX, Integer size) {
+			Preconditions.checkNotNull(minX, "minX must be supplied before values to deserialize EvenlyDiscretizedFunc");
+			Preconditions.checkNotNull(maxX, "maxX must be supplied before values to deserialize EvenlyDiscretizedFunc");
+			Preconditions.checkNotNull(size, "size must be supplied before values to deserialize EvenlyDiscretizedFunc");
+			return new EvenlyDiscretizedFunc(minX, maxX, size);
+		}
+		
+	}
 
 }
