f31bfb6c521205952c5eaa5b1d66ab8cbc8d812b
Kevin Milner <kmilner@usc.edu>
2022-01-20 09:53:59 -0800

    new methods for caching with default implementations

--- src/main/java/org/opensha/sha/earthquake/faultSysSolution/inversion/InversionConfigurationFactory.java
+++ src/main/java/org/opensha/sha/earthquake/faultSysSolution/inversion/InversionConfigurationFactory.java
@@ -1,5 +1,7 @@
 package org.opensha.sha.earthquake.faultSysSolution.inversion;
 
+import java.io.File;
+
 import org.opensha.commons.logicTree.LogicTreeBranch;
 import org.opensha.sha.earthquake.faultSysSolution.FaultSystemRupSet;
 import org.opensha.sha.earthquake.faultSysSolution.modules.SolutionLogicTree;
@@ -43,5 +45,23 @@ public interface InversionConfigurationFactory {
 	public default SolutionProcessor getSolutionLogicTreeProcessor() {
 		return null;
 	};
+	
+	/**
+	 * Sets the cache directory, useful for storing cache files to accelerate rupture building
+	 * 
+	 * @param cacheDir
+	 */
+	public default void setCacheDir(File cacheDir) {};
+	
+	/**
+	 * @param autoCache if true, cache files will automatically be written out if caches change after rupture set
+	 * building
+	 */
+	public default void setAutoCache(boolean autoCache) {};
+	
+	/**
+	 * Writes any relevant cache data for later reuse, default implementation does nothing.
+	 */
+	public default void writeCache() {};
 
 }
