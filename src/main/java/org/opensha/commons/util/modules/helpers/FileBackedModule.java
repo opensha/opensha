00906d75e229f136a136fe8159ae18d0974e14b3
Kevin Milner <kmilner@usc.edu>
2022-03-01 11:29:16 -0800

    increased default buffer size for faster loading

--- src/main/java/org/opensha/commons/util/modules/helpers/FileBackedModule.java
+++ src/main/java/org/opensha/commons/util/modules/helpers/FileBackedModule.java
@@ -83,13 +83,15 @@ public interface FileBackedModule extends ArchivableModule {
 		return zip.getEntry(entryName) != null;
 	}
 	
+	public static final int DEFAULT_BUFFER_SIZE = 1024 * 32 * 8; // 32 kBytes
+	
 	public static BufferedInputStream getInputStream(ZipFile zip, String entryPrefix, String fileName) throws IOException {
 		String entryName = ArchivableModule.getEntryName(entryPrefix, fileName);
 		Preconditions.checkNotNull(entryName, "entryName is null. prefix='%s', fileName='%s'", entryPrefix, fileName);
 		ZipEntry entry = zip.getEntry(entryName);
 		Preconditions.checkNotNull(entry, "Entry not found in zip archive: %s", entryName);
 		
-		return new BufferedInputStream(zip.getInputStream(entry));
+		return new BufferedInputStream(zip.getInputStream(entry), DEFAULT_BUFFER_SIZE);
 	}
 	/**
 	 * Initialize this module from the contents of the file (via this stream)
