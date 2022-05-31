4888bcb83a76bf6d4921f127c01abc5cf9c3a31a
Kevin Milner <kmilner@usc.edu>
2022-05-31 12:13:52 -0700

    added connection rate plot

--- src/main/java/org/opensha/sha/earthquake/faultSysSolution/reports/plots/RateDistributionPlot.java
+++ src/main/java/org/opensha/sha/earthquake/faultSysSolution/reports/plots/RateDistributionPlot.java
@@ -9,6 +9,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
 import org.jfree.chart.ui.RectangleEdge;
 import org.jfree.data.Range;
@@ -40,6 +41,7 @@ import org.opensha.sha.earthquake.faultSysSolution.reports.AbstractSolutionPlot;
 import org.opensha.sha.earthquake.faultSysSolution.reports.ReportMetadata;
 import org.opensha.sha.earthquake.faultSysSolution.reports.RupSetMetadata;
 import org.opensha.sha.earthquake.faultSysSolution.ruptures.ClusterRupture;
+import org.opensha.sha.earthquake.faultSysSolution.ruptures.Jump;
 import org.opensha.sha.earthquake.faultSysSolution.ruptures.util.UniqueRupture;
 
 import com.google.common.base.Preconditions;
@@ -243,171 +245,205 @@ public class RateDistributionPlot extends AbstractSolutionPlot {
 		lines.add("");
 		lines.add("![Cumulative Rate Distribution]("+relPathToResources+"/rate_dist_cumulative.png)");
 		
-		if (compSol == null)
-			return lines;
+		FaultSystemRupSet rupSet = sol.getRupSet();
 		
-		int numRups = meta.primary.numRuptures;
-		if (!meta.primary.rupSet.isEquivalentTo(compSol.getRupSet())) {
-			// see if they're actually the same, but in a different order
-			if (numRups != compSol.getRupSet().getNumRuptures())
-				return lines;
-			compSol = getRemapped(sol.getRupSet(), compSol);
-			if (compSol == null)
-				return lines;
+		if (compSol != null) {
+			int numRups = meta.primary.numRuptures;
+			if (!meta.primary.rupSet.isEquivalentTo(compSol.getRupSet())) {
+				// see if they're actually the same, but in a different order
+				if (numRups != compSol.getRupSet().getNumRuptures())
+					return lines;
+				compSol = getRemapped(sol.getRupSet(), compSol);
+				if (compSol == null)
+					return lines;
+			}
+			
+			double minMag = rupSet.getMinMag();
+			double maxMag = rupSet.getMaxMag();
+			List<Range> magRanges = new ArrayList<>();
+			magRanges.add(null);
+			int minBinSize = (int)(rupSet.getNumRuptures()*0.001);
+			double binFloor = 0;
+			double binCeil = Math.ceil(minMag);
+//			System.out.println("Min bin size: "+minBinSize);
+			while (binFloor < maxMag) {
+				Range range = new Range(binFloor, binCeil);
+				int numInRange = numInRange(range, rupSet.getMagForAllRups());
+//				System.out.println("Potential range ["+(int)range.getLowerBound()+","+(int)range.getUpperBound()+"] has "+numInRange+" rups");
+				if (binFloor == 0d) {
+					// see if we should skip this
+					if (numInRange < minBinSize) {
+						binCeil += 1d;
+//						System.out.println("\tskipping (too small)");
+						continue;
+					}
+				} else if (binCeil > maxMag) {
+					// see if we should combine this with the previous one
+					if (numInRange < minBinSize && magRanges.size() > 1) {
+//						System.out.println("\tskipping/ending (too small)");
+						Range prev = magRanges.remove(magRanges.size()-1);
+						magRanges.add(new Range(prev.getLowerBound(), binCeil));
+						break;
+					}
+				}
+				
+				magRanges.add(range);
+				
+				binFloor = binCeil;
+				binCeil += 1d;
+			}
+			
+			List<XY_DataSet> scatters = new ArrayList<>();
+			for (int i=0; i<magRanges.size(); i++)
+				scatters.add(new DefaultXY_DataSet());
+			
+			MinMaxAveTracker rateTrack = new MinMaxAveTracker();
+			for (int r=0; r<numRups; r++) {
+				double x = Math.max(1e-16, sol.getRateForRup(r));
+				double y = Math.max(1e-16, compSol.getRateForRup(r));
+				for (int m=0; m<magRanges.size(); m++) {
+					Range range = magRanges.get(m);
+					if (range == null || range.contains(rupSet.getMagForRup(r)))
+						scatters.get(m).set(x, y);
+				}
+				rateTrack.addValue(x);
+				rateTrack.addValue(y);
+			}
+			Range range = new Range(Math.max(1e-16, Math.pow(10, Math.floor(Math.log10(rateTrack.getMin())))),
+					Math.pow(10, Math.ceil(Math.log10(rateTrack.getMax()))));
+			
+			table = MarkdownUtils.tableBuilder();
+			for (int m=0; m<magRanges.size(); m++) {
+				Range magRange = magRanges.get(m);
+				
+				String title;
+				String prefix;
+				if (magRange == null) {
+					title = "Rupture Rate Comparison";
+					prefix = "rate_comparison";
+				} else if (magRange.getLowerBound() == 0d) {
+					title = "M≤"+(int)magRange.getUpperBound()+" Rupture Rate Comparison";
+					prefix = "rate_comparison_m_lt_"+(int)magRange.getUpperBound();
+				} else if (magRange.getUpperBound() >= maxMag) {
+					title = "M≥"+(int)magRange.getLowerBound()+" Rupture Rate Comparison";
+					prefix = "rate_comparison_m_ge_"+(int)magRange.getLowerBound();
+				} else {
+					title = "M"+(int)magRange.getLowerBound()+"-"+(int)magRange.getUpperBound()+" Rupture Rate Comparison";
+					prefix = "rate_comparison_m_"+(int)magRange.getLowerBound()+"_"+(int)magRange.getUpperBound();
+				}
+				
+				List<XY_DataSet> funcs = new ArrayList<>();
+				List<PlotCurveCharacterstics> chars = new ArrayList<>();
+				
+				DefaultXY_DataSet oneToOne = new DefaultXY_DataSet();
+				oneToOne.set(range.getLowerBound(), range.getLowerBound());
+				oneToOne.set(range.getUpperBound(), range.getUpperBound());
+
+				XY_DataSet scatter = scatters.get(m);
+				funcs.add(scatter);
+				chars.add(new PlotCurveCharacterstics(PlotSymbol.CROSS, 4f, Color.BLACK));
+				
+				funcs.add(oneToOne);
+				chars.add(new PlotCurveCharacterstics(PlotLineType.DASHED, 3f, Color.GRAY));
+				
+				PlotSpec spec = new PlotSpec(funcs, chars, title,
+						getTruncatedTitle(meta.primary.name)+" Rate", getTruncatedTitle(meta.comparison.name)+" Rate");
+				HeadlessGraphPanel gp = PlotUtils.initHeadless();
+				
+				gp.drawGraphPanel(spec, true, true, range, range);
+				
+				PlotUtils.writePlots(resourcesDir, prefix, gp, 1000, false, true, false, false);
+				
+				double logMin = Math.log10(range.getLowerBound());
+				double logMax = Math.log10(range.getUpperBound());
+				oneToOne = new DefaultXY_DataSet();
+				oneToOne.set(logMin, logMin);
+				oneToOne.set(logMax, logMax);
+				funcs = new ArrayList<>();
+				chars = new ArrayList<>();
+				funcs.add(oneToOne);
+				chars.add(new PlotCurveCharacterstics(PlotLineType.DASHED, 3f, Color.GRAY));
+				HistogramFunction refFunc = HistogramFunction.getEncompassingHistogram(logMin+0.01, logMax-0.01, 0.2);
+				EvenlyDiscrXYZ_DataSet xyz = new EvenlyDiscrXYZ_DataSet(refFunc.size(), refFunc.size(),
+						refFunc.getMinX(), refFunc.getMinX(), refFunc.getDelta());
+				
+				for (Point2D pt : scatter) {
+					double logX = Math.log10(pt.getX());
+					double logY = Math.log10(pt.getY());
+					int xInd = refFunc.getClosestXIndex(logX);
+					int yInd = refFunc.getClosestXIndex(logY);
+					xyz.set(xInd, yInd, xyz.get(xInd, yInd)+1);
+				}
+				
+				// set all zero to NaN so that it will plot clear
+				for (int i=0; i<xyz.size(); i++) {
+					if (xyz.get(i) == 0)
+						xyz.set(i, Double.NaN);
+				}
+				
+				xyz.log10();
+				double maxZ = xyz.getMaxZ();
+				CPT cpt = GMT_CPT_Files.BLACK_RED_YELLOW_UNIFORM.instance().reverse().rescale(0d, Math.ceil(maxZ));
+				cpt.setNanColor(new Color(255, 255, 255, 0));
+				cpt.setBelowMinColor(cpt.getNaNColor());
+				
+				XYZPlotSpec xyzSpec = new XYZPlotSpec(xyz, cpt, title, "Log10 "+spec.getXAxisLabel(),
+						"Log10 "+spec.getYAxisLabel(), "Log10 Count");
+				xyzSpec.setCPTPosition(RectangleEdge.BOTTOM);
+				xyzSpec.setXYElems(funcs);
+				xyzSpec.setXYChars(chars);
+				Range logRange = new Range(refFunc.getMinX()-0.5*refFunc.getDelta(), refFunc.getMaxX()+0.5*refFunc.getDelta());
+				gp.drawGraphPanel(xyzSpec, false, false, logRange, logRange);
+				
+				gp.getChartPanel().setSize(1000, 1000);
+				gp.saveAsPNG(new File(resourcesDir, prefix+"_hist2D.png").getAbsolutePath());
+				
+				table.initNewLine();
+				table.addColumn("![rate comparison]("+relPathToResources+"/"+prefix+".png)");
+				table.addColumn("![rate hist]("+relPathToResources+"/"+prefix+"_hist2D.png)");
+				table.finalizeLine();
+			}
+			
+			lines.add("");
+			lines.add(getSubHeading()+" Rupture Rate Comparison");
+			lines.add(topLink); lines.add("");
+			
+			lines.addAll(table.build());
 		}
 		
-		FaultSystemRupSet rupSet = sol.getRupSet();
-		double minMag = rupSet.getMinMag();
-		double maxMag = rupSet.getMaxMag();
-		List<Range> magRanges = new ArrayList<>();
-		magRanges.add(null);
-		int minBinSize = (int)(rupSet.getNumRuptures()*0.001);
-		double binFloor = 0;
-		double binCeil = Math.ceil(minMag);
-//		System.out.println("Min bin size: "+minBinSize);
-		while (binFloor < maxMag) {
-			Range range = new Range(binFloor, binCeil);
-			int numInRange = numInRange(range, rupSet.getMagForAllRups());
-//			System.out.println("Potential range ["+(int)range.getLowerBound()+","+(int)range.getUpperBound()+"] has "+numInRange+" rups");
-			if (binFloor == 0d) {
-				// see if we should skip this
-				if (numInRange < minBinSize) {
-					binCeil += 1d;
-//					System.out.println("\tskipping (too small)");
-					continue;
-				}
-			} else if (binCeil > maxMag) {
-				// see if we should combine this with the previous one
-				if (numInRange < minBinSize && magRanges.size() > 1) {
-//					System.out.println("\tskipping/ending (too small)");
-					Range prev = magRanges.remove(magRanges.size()-1);
-					magRanges.add(new Range(prev.getLowerBound(), binCeil));
+		if (rupSet.hasModule(ClusterRuptures.class)) {
+			boolean jumpFound = false;
+			ClusterRuptures cRups = rupSet.requireModule(ClusterRuptures.class);
+			for (ClusterRupture cRup : cRups) {
+				if (cRup.getTotalNumJumps() > 0) {
+					jumpFound = true;
 					break;
 				}
 			}
-			
-			magRanges.add(range);
-			
-			binFloor = binCeil;
-			binCeil += 1d;
+			if (jumpFound) {
+				// add jump rate vs rank plot
+				double[] jumpRates = calcJumpRates(rupSet, sol.getRateForAllRups());
+				if (jumpRates == null || jumpRates.length < 2)
+					return lines;
+				
+				double[] initialJumpRates = sol.hasModule(InitialSolution.class) ?
+						calcJumpRates(rupSet, initial) : new double[jumpRates.length];
+				
+				double[] compJumpRates = null;
+				if (compSol != null)
+					compJumpRates = calcJumpRates(compSol.getRupSet(), compSol.getRateForAllRups());
+				
+				lines.add(getSubHeading()+" Connection Rate Distribution");
+				lines.add(topLink); lines.add("");
+				lines.add("This plot gives the rate that each unique connection (between separate fault sections) is utilized.");
+				lines.add("");
+				
+				SimulatedAnnealing.writeRateVsRankPlot(resourcesDir, "conn_rate_dist", jumpRates, jumpRates,
+						initialJumpRates, compJumpRates, compJumpRates, "Connection Rate Distribution");
+				lines.add("![Connection Rate Distribution]("+relPathToResources+"/conn_rate_dist.png)");
+				lines.add("");
+			}
 		}
-		
-		List<XY_DataSet> scatters = new ArrayList<>();
-		for (int i=0; i<magRanges.size(); i++)
-			scatters.add(new DefaultXY_DataSet());
-		
-		MinMaxAveTracker rateTrack = new MinMaxAveTracker();
-		for (int r=0; r<numRups; r++) {
-			double x = Math.max(1e-16, sol.getRateForRup(r));
-			double y = Math.max(1e-16, compSol.getRateForRup(r));
-			for (int m=0; m<magRanges.size(); m++) {
-				Range range = magRanges.get(m);
-				if (range == null || range.contains(rupSet.getMagForRup(r)))
-					scatters.get(m).set(x, y);
-			}
-			rateTrack.addValue(x);
-			rateTrack.addValue(y);
-		}
-		Range range = new Range(Math.max(1e-16, Math.pow(10, Math.floor(Math.log10(rateTrack.getMin())))),
-				Math.pow(10, Math.ceil(Math.log10(rateTrack.getMax()))));
-		
-		table = MarkdownUtils.tableBuilder();
-		for (int m=0; m<magRanges.size(); m++) {
-			Range magRange = magRanges.get(m);
-			
-			String title;
-			String prefix;
-			if (magRange == null) {
-				title = "Rupture Rate Comparison";
-				prefix = "rate_comparison";
-			} else if (magRange.getLowerBound() == 0d) {
-				title = "M≤"+(int)magRange.getUpperBound()+" Rupture Rate Comparison";
-				prefix = "rate_comparison_m_lt_"+(int)magRange.getUpperBound();
-			} else if (magRange.getUpperBound() >= maxMag) {
-				title = "M≥"+(int)magRange.getLowerBound()+" Rupture Rate Comparison";
-				prefix = "rate_comparison_m_ge_"+(int)magRange.getLowerBound();
-			} else {
-				title = "M"+(int)magRange.getLowerBound()+"-"+(int)magRange.getUpperBound()+" Rupture Rate Comparison";
-				prefix = "rate_comparison_m_"+(int)magRange.getLowerBound()+"_"+(int)magRange.getUpperBound();
-			}
-			
-			List<XY_DataSet> funcs = new ArrayList<>();
-			List<PlotCurveCharacterstics> chars = new ArrayList<>();
-			
-			DefaultXY_DataSet oneToOne = new DefaultXY_DataSet();
-			oneToOne.set(range.getLowerBound(), range.getLowerBound());
-			oneToOne.set(range.getUpperBound(), range.getUpperBound());
-
-			XY_DataSet scatter = scatters.get(m);
-			funcs.add(scatter);
-			chars.add(new PlotCurveCharacterstics(PlotSymbol.CROSS, 4f, Color.BLACK));
-			
-			funcs.add(oneToOne);
-			chars.add(new PlotCurveCharacterstics(PlotLineType.DASHED, 3f, Color.GRAY));
-			
-			PlotSpec spec = new PlotSpec(funcs, chars, title,
-					getTruncatedTitle(meta.primary.name)+" Rate", getTruncatedTitle(meta.comparison.name)+" Rate");
-			HeadlessGraphPanel gp = PlotUtils.initHeadless();
-			
-			gp.drawGraphPanel(spec, true, true, range, range);
-			
-			PlotUtils.writePlots(resourcesDir, prefix, gp, 1000, false, true, false, false);
-			
-			double logMin = Math.log10(range.getLowerBound());
-			double logMax = Math.log10(range.getUpperBound());
-			oneToOne = new DefaultXY_DataSet();
-			oneToOne.set(logMin, logMin);
-			oneToOne.set(logMax, logMax);
-			funcs = new ArrayList<>();
-			chars = new ArrayList<>();
-			funcs.add(oneToOne);
-			chars.add(new PlotCurveCharacterstics(PlotLineType.DASHED, 3f, Color.GRAY));
-			HistogramFunction refFunc = HistogramFunction.getEncompassingHistogram(logMin+0.01, logMax-0.01, 0.2);
-			EvenlyDiscrXYZ_DataSet xyz = new EvenlyDiscrXYZ_DataSet(refFunc.size(), refFunc.size(),
-					refFunc.getMinX(), refFunc.getMinX(), refFunc.getDelta());
-			
-			for (Point2D pt : scatter) {
-				double logX = Math.log10(pt.getX());
-				double logY = Math.log10(pt.getY());
-				int xInd = refFunc.getClosestXIndex(logX);
-				int yInd = refFunc.getClosestXIndex(logY);
-				xyz.set(xInd, yInd, xyz.get(xInd, yInd)+1);
-			}
-			
-			// set all zero to NaN so that it will plot clear
-			for (int i=0; i<xyz.size(); i++) {
-				if (xyz.get(i) == 0)
-					xyz.set(i, Double.NaN);
-			}
-			
-			xyz.log10();
-			double maxZ = xyz.getMaxZ();
-			CPT cpt = GMT_CPT_Files.BLACK_RED_YELLOW_UNIFORM.instance().reverse().rescale(0d, Math.ceil(maxZ));
-			cpt.setNanColor(new Color(255, 255, 255, 0));
-			cpt.setBelowMinColor(cpt.getNaNColor());
-			
-			XYZPlotSpec xyzSpec = new XYZPlotSpec(xyz, cpt, title, "Log10 "+spec.getXAxisLabel(),
-					"Log10 "+spec.getYAxisLabel(), "Log10 Count");
-			xyzSpec.setCPTPosition(RectangleEdge.BOTTOM);
-			xyzSpec.setXYElems(funcs);
-			xyzSpec.setXYChars(chars);
-			Range logRange = new Range(refFunc.getMinX()-0.5*refFunc.getDelta(), refFunc.getMaxX()+0.5*refFunc.getDelta());
-			gp.drawGraphPanel(xyzSpec, false, false, logRange, logRange);
-			
-			gp.getChartPanel().setSize(1000, 1000);
-			gp.saveAsPNG(new File(resourcesDir, prefix+"_hist2D.png").getAbsolutePath());
-			
-			table.initNewLine();
-			table.addColumn("![rate comparison]("+relPathToResources+"/"+prefix+".png)");
-			table.addColumn("![rate hist]("+relPathToResources+"/"+prefix+"_hist2D.png)");
-			table.finalizeLine();
-		}
-		
-		lines.add("");
-		lines.add(getSubHeading()+" Rupture Rate Comparison");
-		lines.add(topLink); lines.add("");
-		
-		lines.addAll(table.build());
 		
 		return lines;
 	}
@@ -461,6 +497,35 @@ public class RateDistributionPlot extends AbstractSolutionPlot {
 		return count;
 	}
 	
+	private static double[] calcJumpRates(FaultSystemRupSet rupSet, double[] rates) {
+		Map<Jump, Double> jumpRates = new HashMap<>();
+		
+		ClusterRuptures cRups = rupSet.requireModule(ClusterRuptures.class);
+		for (int rupIndex=0; rupIndex<rupSet.getNumRuptures(); rupIndex++) {
+			double rate = rates[rupIndex];
+			if (rate == 0d)
+				continue;
+			ClusterRupture cRup = cRups.get(rupIndex);
+			for (Jump jump : cRup.getJumpsIterable()) {
+				if (jump.fromSection.getSectionId() > jump.toSection.getSectionId())
+					jump = jump.reverse();
+				if (jumpRates.containsKey(jump))
+					jumpRates.put(jump, jumpRates.get(jump)+rate);
+				else
+					jumpRates.put(jump, rate);
+			}
+		}
+		
+		if (jumpRates.isEmpty())
+			return null;
+		
+		double[] ret = new double[jumpRates.size()];
+		int index = 0;
+		for (Jump jump : jumpRates.keySet())
+			ret[index++] = jumpRates.get(jump);
+		return ret;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		File mainDir = new File("/home/kevin/markdown/inversions");
 		FaultSystemSolution sol1 = FaultSystemSolution.load(
@@ -472,6 +537,14 @@ public class RateDistributionPlot extends AbstractSolutionPlot {
 						+ "2021_10_19-reproduce-ucerf3-ref_branch-tapered-convergence-u3Iters/"
 						+ "FM3_1_ZENGBB_Shaw09Mod_DsrTap_CharConst_M5Rate7.9_MMaxOff7.6_NoFix_SpatSeisU3_mean_sol.zip"));
 //				new File(mainDir, "2021_08_05-coulomb-u3_ref-perturb_exp_scale_1e-2_to_1e-10-avg_anneal_20m-wlAsInitial-5hr-run1/solution.zip"));
+		
+		sol1.getRupSet().addAvailableModule(new Callable<ClusterRuptures>() {
+
+			@Override
+			public ClusterRuptures call() throws Exception {
+				return ClusterRuptures.singleStranged(sol1.getRupSet());
+			}
+		}, ClusterRuptures.class);
 	
 		RateDistributionPlot plot = new RateDistributionPlot();
 		ReportMetadata meta = new ReportMetadata(new RupSetMetadata("sol1", sol1), new RupSetMetadata("sol2", sol2));
