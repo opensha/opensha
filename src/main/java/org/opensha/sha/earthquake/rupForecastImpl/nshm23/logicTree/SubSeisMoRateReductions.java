deb312c9490129d7df4539760f9e5a9b521933f7
Kevin Milner <kmilner@usc.edu>
2022-08-18 09:52:58 -0700

    scaling relationship test file from Bruce (currently fails CSD slip)

--- src/main/java/org/opensha/sha/earthquake/rupForecastImpl/nshm23/logicTree/SubSeisMoRateReductions.java
+++ src/main/java/org/opensha/sha/earthquake/rupForecastImpl/nshm23/logicTree/SubSeisMoRateReductions.java
@@ -15,7 +15,7 @@ import org.opensha.sha.earthquake.rupForecastImpl.nshm23.targetMFDs.SupraSeisBVa
 public enum SubSeisMoRateReductions implements LogicTreeNode {
 	FAULT_SPECIFIC("Fault-Specific", "FaultSpec", 0d, SubSeisMoRateReduction.FAULT_SPECIFIC_IMPLIED_FROM_SUPRA_B),
 	SYSTEM_AVG("System-Average", "SysAvg", 0d, SubSeisMoRateReduction.SYSTEM_AVG_IMPLIED_FROM_SUPRA_B),
-	NONE("None", "NoRed", 0d, SubSeisMoRateReduction.NONE),
+	NONE("None", "NoRed", 1d, SubSeisMoRateReduction.NONE),
 	SUB_B_1("Sub-Seis b=1", "SubB1", 1d, SubSeisMoRateReduction.SUB_SEIS_B_1),
 	SYSTEM_AVG_SUB_B_1("System-Average Sub-Seis b=1", "SysAvgSubB1", 0d, SubSeisMoRateReduction.SYSTEM_AVG_SUB_B_1);
 	
