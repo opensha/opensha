5c7eb94fdbc5a4b5d44ccb7735269373bdcddeae
Kevin Milner <kmilner@usc.edu>
2022-12-15 09:59:50 -0800

    now supports average girdded seismicity

--- src/main/java/org/opensha/sha/earthquake/faultSysSolution/hazard/FaultAndGriddedSeparateTreeHazardCombiner.java
+++ src/main/java/org/opensha/sha/earthquake/faultSysSolution/hazard/FaultAndGriddedSeparateTreeHazardCombiner.java
@@ -1,6 +1,5 @@
 package org.opensha.sha.earthquake.faultSysSolution.hazard;
 
-import java.awt.geom.Point2D;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -10,7 +9,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.StringWriter;
 import java.text.DecimalFormat;
-import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -28,12 +26,12 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntryPredicate;
 import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
-import org.netlib.util.intW;
 import org.opensha.commons.data.CSVFile;
-import org.opensha.commons.data.function.ArbitrarilyDiscretizedFunc;
 import org.opensha.commons.data.function.DiscretizedFunc;
 import org.opensha.commons.data.function.LightFixedXFunc;
 import org.opensha.commons.data.xyz.ArbDiscrGeoDataSet;
@@ -60,6 +58,9 @@ import com.google.common.primitives.Doubles;
 public class FaultAndGriddedSeparateTreeHazardCombiner {
 
 	public static void main(String[] args) throws IOException {
+		CommandLine cmd = FaultSysTools.parseOptions(createOptions(), args, LogicTreeHazardCompare.class);
+		args = cmd.getArgs();
+		
 		if (args.length < 7 || args.length > 8) {
 			System.err.println("USAGE: <results_fault.zip> <results_fault_hazard_dir> "
 					+ "<results_gridded.zip> <results_gridded_hazard_dir> "
@@ -76,7 +77,8 @@ public class FaultAndGriddedSeparateTreeHazardCombiner {
 		SolutionLogicTree griddedSLT = SolutionLogicTree.load(new File(args[cnt++]));
 		File griddedHazardDir = new File(args[cnt++]);
 		
-		File resultsOutFile = new File(args[cnt++]);
+		String resultFileName = args[cnt++];
+		File resultsOutFile = resultFileName.trim().toLowerCase().equals("null") ? null : new File(resultFileName);
 		File hazardOutFile = new File(args[cnt++]);
 		
 //		double gridSpacing = Double.parseDouble(args[cnt++]);
@@ -115,21 +117,6 @@ public class FaultAndGriddedSeparateTreeHazardCombiner {
 		LogicTree<?> faultTree = faultSLT.getLogicTree();
 		LogicTree<?> gridTree = griddedSLT.getLogicTree();
 		
-		List<LogicTreeLevel<? extends LogicTreeNode>> combinedLevels = new ArrayList<>();
-		combinedLevels.addAll(faultTree.getLevels());
-		for (LogicTreeLevel<?> gridLevel : gridTree.getLevels()) {
-			// see if it's already in the fault tree
-			boolean duplicate = false;
-			for (LogicTreeLevel<?> faultLevel : faultTree.getLevels()) {
-				if (faultLevel.getType().equals(gridLevel.getType())) {
-					duplicate = true;
-					break;
-				}
-			}
-			if (!duplicate)
-				combinedLevels.add(gridLevel);
-		}
-		
 		String faultHazardDirName = "hazard_"+(float)gridSpacing+"deg_grid_seis_"+IncludeBackgroundOption.EXCLUDE.name();
 		String gridHazardDirName = "hazard_"+(float)gridSpacing+"deg_grid_seis_"+IncludeBackgroundOption.ONLY.name();
 		
@@ -142,7 +129,46 @@ public class FaultAndGriddedSeparateTreeHazardCombiner {
 			File branchHazardDir = new File(branchResultsDir, gridHazardDirName);
 			Preconditions.checkState(branchHazardDir.exists(), "%s doesn't exist", branchHazardDir.getAbsolutePath());
 			
-			gridSeisCurves.add(loadCurves(branchHazardDir, periods));
+			gridSeisCurves.add(loadCurves(branchHazardDir, periods, gridReg));
+		}
+		
+		DiscretizedFunc[][] meanGridSeisCurves = null;
+		List<LogicTreeLevel<? extends LogicTreeNode>> combinedLevels = new ArrayList<>();
+		combinedLevels.addAll(faultTree.getLevels());
+		if (cmd.hasOption("average-gridded")) {
+			System.out.println("Averaging gridded seismicity curves");
+			LogicTreeCurveAverager[] meanCurves = new LogicTreeCurveAverager[periods.length];
+			for (int p=0; p<periods.length; p++)
+				meanCurves[p] = new LogicTreeCurveAverager(gridTree, gridReg.getNodeList());
+			
+			for (int g=0; g<gridTree.size(); g++) {
+				LogicTreeBranch<?> gridBranch = gridTree.getBranch(g);
+				DiscretizedFunc[][] gridCurves = gridSeisCurves.get(g);
+				
+				double weight = gridTree.getBranchWeight(g);
+				
+				for (int p=0; p<periods.length; p++)
+					meanCurves[p].processBranchCurves(gridBranch, weight, gridCurves[p]);
+			}
+			
+			meanGridSeisCurves = new DiscretizedFunc[periods.length][];
+			for (int p=0; p<periods.length; p++)
+				meanGridSeisCurves[p] = meanCurves[p].getNormalizedCurves().get(LogicTreeCurveAverager.MEAN_PREFIX);
+			
+			gridTree = null;
+		} else {
+			for (LogicTreeLevel<?> gridLevel : gridTree.getLevels()) {
+				// see if it's already in the fault tree
+				boolean duplicate = false;
+				for (LogicTreeLevel<?> faultLevel : faultTree.getLevels()) {
+					if (faultLevel.getType().equals(gridLevel.getType())) {
+						duplicate = true;
+						break;
+					}
+				}
+				if (!duplicate)
+					combinedLevels.add(gridLevel);
+			}
 		}
 		
 		List<LogicTreeBranch<LogicTreeNode>> combinedBranches = new ArrayList<>();
@@ -156,7 +182,8 @@ public class FaultAndGriddedSeparateTreeHazardCombiner {
 		HashSet<LogicTreeNode> variableNodes = new HashSet<>();
 		HashMap<LogicTreeNode, LogicTreeLevel<?>> nodeLevels = new HashMap<>();
 		LogicTreeCurveAverager.populateVariableNodes(faultTree, variableNodes, nodeLevels);
-		LogicTreeCurveAverager.populateVariableNodes(gridTree, variableNodes, nodeLevels);
+		if (gridTree != null)
+			LogicTreeCurveAverager.populateVariableNodes(gridTree, variableNodes, nodeLevels);
 		for (int p=0; p<periods.length; p++)
 			meanCurves[p] = new LogicTreeCurveAverager(gridReg.getNodeList(), variableNodes, nodeLevels);
 		
@@ -188,22 +215,29 @@ public class FaultAndGriddedSeparateTreeHazardCombiner {
 			
 			List<CompletableFuture<Void>> perAvgFutures = new ArrayList<>();
 			
-			for (int g=0; g<gridTree.size(); g++) {
+			int numGrid = meanGridSeisCurves != null ? 1 : gridTree.size();
+			for (int g=0; g<numGrid; g++) {
 				combineWatch.start();
-				LogicTreeBranch<?> gridBranch = gridTree.getBranch(g);
+				
 				LogicTreeBranch<LogicTreeNode> combinedBranch = new LogicTreeBranch<>(combinedLevels);
 				for (LogicTreeNode node : faultBranch)
 					combinedBranch.setValue(node);
-				for (LogicTreeNode node : gridBranch)
-					combinedBranch.setValue(node);
-				
-				double gridWeight = gridTree.getBranchWeight(gridBranch);
+				DiscretizedFunc[][] gridCurves;
+				double gridWeight;
+				if (meanGridSeisCurves == null) {
+					LogicTreeBranch<?> gridBranch = gridTree.getBranch(g);
+					for (LogicTreeNode node : gridBranch)
+						combinedBranch.setValue(node);
+					
+					gridWeight = gridTree.getBranchWeight(gridBranch);
+					gridCurves = gridSeisCurves.get(g);
+				} else {
+					gridWeight = 1d;
+					gridCurves = meanGridSeisCurves;
+				}
 				double combWeight = faultWeight * gridWeight;
-				
-				combinedBranch.setOrigBranchWeight(combWeight);
-				
-				DiscretizedFunc[][] gridCurves = gridSeisCurves.get(g);
 				String combPrefix = combinedBranch.buildFileName();
+				combinedBranch.setOrigBranchWeight(faultWeight);
 				
 				Map<String, GriddedGeoDataSet> writeMap = new HashMap<>(gridReg.getNodeCount()*periods.length);
 				
@@ -380,40 +414,51 @@ public class FaultAndGriddedSeparateTreeHazardCombiner {
 		hazardOutZip.close();
 		Files.move(tmpOut, hazardOutFile);
 		
-		// now copy over results zip file with the new tree
-		tmpOut = new File(resultsOutFile.getAbsolutePath()+".tmp");
-		ZipArchiveOutputStream zout = new ZipArchiveOutputStream(tmpOut);
-		
-		org.apache.commons.compress.archivers.zip.ZipFile zip = new org.apache.commons.compress.archivers.zip.ZipFile(faultSLTFile);
-		
-		zip.copyRawEntries(zout, new ZipArchiveEntryPredicate() {
+		if (resultsOutFile != null) {
+			// now copy over results zip file with the new tree
+			tmpOut = new File(resultsOutFile.getAbsolutePath()+".tmp");
+			ZipArchiveOutputStream zout = new ZipArchiveOutputStream(tmpOut);
 			
-			@Override
-			public boolean test(ZipArchiveEntry zipArchiveEntry) {
-				if (zipArchiveEntry.getName().endsWith("logic_tree.json")) {
-					// update it
-					try {
-						LogicTree<?> tree = LogicTree.fromExisting(combinedLevels, combinedBranches);
-						System.out.println("Updaing logic tree archive with "+tree.size()+" branches: "+zipArchiveEntry.getName());
-						
-						zout.putArchiveEntry(new ZipArchiveEntry(zipArchiveEntry.getName()));
-						BufferedOutputStream bStream = new BufferedOutputStream(zout);
-						tree.writeToStream(bStream);
-						bStream.flush();
-						zout.flush();
-						zout.closeArchiveEntry();
-					} catch (IOException e) {
-						throw ExceptionUtils.asRuntimeException(e);
+			org.apache.commons.compress.archivers.zip.ZipFile zip = new org.apache.commons.compress.archivers.zip.ZipFile(faultSLTFile);
+			
+			zip.copyRawEntries(zout, new ZipArchiveEntryPredicate() {
+				
+				@Override
+				public boolean test(ZipArchiveEntry zipArchiveEntry) {
+					if (zipArchiveEntry.getName().endsWith("logic_tree.json")) {
+						// update it
+						try {
+							LogicTree<?> tree = LogicTree.fromExisting(combinedLevels, combinedBranches);
+							System.out.println("Updaing logic tree archive with "+tree.size()+" branches: "+zipArchiveEntry.getName());
+							
+							zout.putArchiveEntry(new ZipArchiveEntry(zipArchiveEntry.getName()));
+							BufferedOutputStream bStream = new BufferedOutputStream(zout);
+							tree.writeToStream(bStream);
+							bStream.flush();
+							zout.flush();
+							zout.closeArchiveEntry();
+						} catch (IOException e) {
+							throw ExceptionUtils.asRuntimeException(e);
+						}
+						return false;
 					}
-					return false;
+					return true;
 				}
-				return true;
-			}
-		});
+			});
+			
+			zip.close();
+			zout.close();
+			Files.move(tmpOut, resultsOutFile);
+		}
+	}
+	
+	public static Options createOptions() {
+		Options ops = new Options();
 		
-		zip.close();
-		zout.close();
-		Files.move(tmpOut, resultsOutFile);
+		ops.addOption("ag", "average-gridded", false,
+				"Flag to only use average gridded seismicity.");
+		
+		return ops;
 	}
 	
 	private static final DecimalFormat twoDigits = new DecimalFormat("0.00");
@@ -446,7 +491,7 @@ public class FaultAndGriddedSeparateTreeHazardCombiner {
 			@Override
 			public DiscretizedFunc[][] get() {
 				try {
-					return loadCurves(branchHazardDir, periods);
+					return loadCurves(branchHazardDir, periods, null);
 				} catch (IOException e) {
 					throw ExceptionUtils.asRuntimeException(e);
 				}
@@ -454,7 +499,7 @@ public class FaultAndGriddedSeparateTreeHazardCombiner {
 		});
 	}
 	
-	private static DiscretizedFunc[][] loadCurves(File hazardDir, double[] periods) throws IOException {
+	private static DiscretizedFunc[][] loadCurves(File hazardDir, double[] periods, GriddedRegion region) throws IOException {
 		DiscretizedFunc[][] curves = new DiscretizedFunc[periods.length][];
 		for (int p=0; p<periods.length; p++) {
 			String fileName = SolHazardMapCalc.getCSV_FileName("curves", periods[p]);
@@ -463,7 +508,7 @@ public class FaultAndGriddedSeparateTreeHazardCombiner {
 				csvFile = new File(hazardDir, fileName+".gz");
 			Preconditions.checkState(csvFile.exists(), "Curves CSV file not found: %s", csvFile.getAbsolutePath());
 			CSVFile<String> csv = CSVFile.readFile(csvFile, true);
-			curves[p] = SolHazardMapCalc.loadCurvesCSV(csv, null);
+			curves[p] = SolHazardMapCalc.loadCurvesCSV(csv, region);
 		}
 		return curves;
 	}
