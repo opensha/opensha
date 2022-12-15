fd40d319d5000ae081df348b87ea447bb845145c
Kevin Milner <kmilner@usc.edu>
2022-12-15 10:04:10 -0800

    serialization fixes

--- src/main/java/org/opensha/sha/earthquake/faultSysSolution/ruptures/plausibility/impl/prob/RuptureProbabilityCalc.java
+++ src/main/java/org/opensha/sha/earthquake/faultSysSolution/ruptures/plausibility/impl/prob/RuptureProbabilityCalc.java
@@ -1,11 +1,22 @@
 package org.opensha.sha.earthquake.faultSysSolution.ruptures.plausibility.impl.prob;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.opensha.commons.data.Named;
+import org.opensha.commons.util.ExceptionUtils;
 import org.opensha.sha.earthquake.faultSysSolution.ruptures.ClusterRupture;
 import org.opensha.sha.earthquake.faultSysSolution.ruptures.strategies.ClusterConnectionStrategy;
 import org.opensha.sha.earthquake.faultSysSolution.ruptures.util.SectionDistanceAzimuthCalculator;
 
 import com.google.common.base.Preconditions;
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
 
 /**
  * Inteface for a rupture probability calculator
@@ -45,6 +56,7 @@ public interface RuptureProbabilityCalc extends Named {
 	
 	public class LogicalAnd implements BinaryRuptureProbabilityCalc {
 		
+		@JsonAdapter(GenericArrayBinaryRupProbCalcAdapter.class)
 		private BinaryRuptureProbabilityCalc[] calcs;
 
 		public LogicalAnd(BinaryRuptureProbabilityCalc... calcs) {
@@ -76,6 +88,7 @@ public interface RuptureProbabilityCalc extends Named {
 	
 	public class MultiProduct implements RuptureProbabilityCalc {
 		
+		@JsonAdapter(GenericArrayRupProbCalcAdapter.class)
 		private RuptureProbabilityCalc[] calcs;
 
 		public MultiProduct(RuptureProbabilityCalc... calcs) {
@@ -102,6 +115,114 @@ public interface RuptureProbabilityCalc extends Named {
 				if (calc.isDirectional(splayed))
 					return true;
 			return false;
+		}
+		
+	}
+	
+	public static class GenericArrayRupProbCalcAdapter extends TypeAdapter<RuptureProbabilityCalc[]> {
+		
+		private GenericRupProbCalcAdapter adapter = new GenericRupProbCalcAdapter();
+
+		@Override
+		public void write(JsonWriter out, RuptureProbabilityCalc[] calcs) throws IOException {
+			out.beginArray();
+			
+			for (RuptureProbabilityCalc calc : calcs)
+				adapter.write(out, calc);
+			
+			out.endArray();
+		}
+
+		@Override
+		public RuptureProbabilityCalc[] read(JsonReader in) throws IOException {
+			in.beginArray();
+			
+			List<RuptureProbabilityCalc> calcs = new ArrayList<>();
+			
+			while (in.hasNext())
+				calcs.add(adapter.read(in));
+			
+			in.endArray();
+			
+			return calcs.toArray(new RuptureProbabilityCalc[0]);
+		}
+		
+	}
+	
+	public static class GenericArrayBinaryRupProbCalcAdapter extends TypeAdapter<BinaryRuptureProbabilityCalc[]> {
+		
+		private GenericRupProbCalcAdapter adapter = new GenericRupProbCalcAdapter();
+
+		@Override
+		public void write(JsonWriter out, BinaryRuptureProbabilityCalc[] calcs) throws IOException {
+			out.beginArray();
+			
+			for (RuptureProbabilityCalc calc : calcs)
+				adapter.write(out, calc);
+			
+			out.endArray();
+		}
+
+		@Override
+		public BinaryRuptureProbabilityCalc[] read(JsonReader in) throws IOException {
+			in.beginArray();
+			
+			List<BinaryRuptureProbabilityCalc> calcs = new ArrayList<>();
+			
+			while (in.hasNext()) {
+				RuptureProbabilityCalc calc = adapter.read(in);
+				Preconditions.checkState(calc instanceof BinaryRuptureProbabilityCalc);
+				calcs.add((BinaryRuptureProbabilityCalc)calc);
+			}
+			
+			in.endArray();
+			
+			return calcs.toArray(new BinaryRuptureProbabilityCalc[0]);
+		}
+		
+	}
+	
+	public static class GenericRupProbCalcAdapter extends TypeAdapter<RuptureProbabilityCalc> {
+		
+		Gson gson = new Gson();
+
+		@Override
+		public void write(JsonWriter out, RuptureProbabilityCalc value) throws IOException {
+			if (value == null) {
+				out.nullValue();
+				return;
+			}
+			out.beginObject();
+
+			out.name("type").value(value.getClass().getName());
+			out.name("data");
+			gson.toJson(value, value.getClass(), out);
+
+			out.endObject();
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public RuptureProbabilityCalc read(JsonReader in) throws IOException {
+			if (in.peek() == JsonToken.NULL)
+				return null;
+			
+			Class<? extends RuptureProbabilityCalc> type = null;
+
+			in.beginObject();
+
+			Preconditions.checkState(in.nextName().equals("type"), "JSON 'type' object must be first");
+			try {
+				type = (Class<? extends RuptureProbabilityCalc>) Class.forName(in.nextString());
+			} catch (ClassNotFoundException | ClassCastException e) {
+				throw ExceptionUtils.asRuntimeException(e);
+			}
+
+			Preconditions.checkState(in.nextName().equals("data"), "JSON 'data' object must be second");
+			RuptureProbabilityCalc model = gson.fromJson(in, type);
+
+			in.endObject();
+			return model;
 		}
 		
 	}
