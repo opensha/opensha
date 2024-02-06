5c2d971b50cc81fb8e617bd494df583da33ab008
Kevin Milner <kmilner@usc.edu>
2024-02-06 14:29:22 -0800

    new CLI, and also tracks branch rupture magnitudes

--- src/main/java/org/opensha/sha/earthquake/faultSysSolution/modules/TrueMeanRuptureMappings.java
+++ src/main/java/org/opensha/sha/earthquake/faultSysSolution/modules/TrueMeanRuptureMappings.java
@@ -29,12 +29,14 @@ public abstract class TrueMeanRuptureMappings extends AbstractLogicTreeModule {
 	
 	public static final String SECT_MAPPING_FILE_NAME = "sect_mappings.csv";
 	public static final String RUP_MAPPING_FILE_NAME = "rup_mappings.csv";
+	public static final String RUP_MAG_FILE_NAME = "rup_mags.csv";
 	
 	protected List<? extends LogicTreeLevel<?>> sectMappingLevels;
 	protected List<LogicTreeLevel<?>> rupMappingLevels;
 	
 	protected Map<String, int[]> sectFileMappingsCache = new HashMap<>();
 	protected Map<String, int[]> rupFileMappingsCache = new HashMap<>();
+	protected Map<String, double[]> rupFileMagsCache = new HashMap<>();
 	
 	private TrueMeanRuptureMappings(ZipFile zip, String prefix, LogicTree<?> logicTree) {
 		super(zip, prefix, logicTree);
@@ -80,6 +82,14 @@ public abstract class TrueMeanRuptureMappings extends AbstractLogicTreeModule {
 		return csv;
 	}
 	
+	private CSVFile<String> buildMappingCSV(String type, String quantity, double[] vals) {
+		CSVFile<String> csv = new CSVFile<>(true);
+		csv.addLine("Branch "+type+" Index", "Branch "+quantity);
+		for (int i=0; i<vals.length; i++)
+			csv.addLine(i+"", vals[i]+"");
+		return csv;
+	}
+	
 	/**
 	 * @param branch
 	 * @return array of of size numSects, where each value is the corresponding section index in the true mean solution 
@@ -91,6 +101,12 @@ public abstract class TrueMeanRuptureMappings extends AbstractLogicTreeModule {
 	 * @return array of of size numRups, where each value is the corresponding rupture index in the true mean solution 
 	 */
 	public abstract int[] getRuptureMappings(LogicTreeBranch<?> branch);
+	
+	/**
+	 * @param branch
+	 * @return array of of size numRups, where each value is the branch-specific magnitude 
+	 */
+	public abstract double[] getRuptureMags(LogicTreeBranch<?> branch);
 
 	@Override
 	protected synchronized Map<String, String> writeBranchFilesToArchive(ZipOutputStream zout, String prefix, LogicTreeBranch<?> branch,
@@ -140,6 +156,27 @@ public abstract class TrueMeanRuptureMappings extends AbstractLogicTreeModule {
 			rupFileMappingsCache.put(rupMappingFile, rupMappings);
 			writtenFiles.add(rupMappingFile);
 		}
+		
+		String rupMagsFile = getRecordBranchFileName(branch, prefix, RUP_MAG_FILE_NAME, rupMappingLevels, mappings);
+		if (writtenFiles.contains(rupMagsFile)) {
+			if (!(this instanceof FileBacked)) {
+				// already written, validate that it is indeed identical
+				// but skip this check if we're just loading it from another archive directly
+				double[] rupMags = getRuptureMags(branch);
+				double[] prev = rupFileMagsCache.get(rupMagsFile);
+				Preconditions.checkNotNull(prev, "Already written %s but prev not cached?", rupMagsFile);
+				Preconditions.checkState(Arrays.equals(rupMags, prev),
+						"Mags not identical for %s, new branch: %s", rupMagsFile, branch);
+			}
+		} else {
+			double[] rupMags = getRuptureMags(branch);
+			CSVFile<String> csv = buildMappingCSV("Rupture", "Magnitude", rupMags);
+			
+			CSV_BackedModule.writeToArchive(csv, zout, entryPrefix, rupMagsFile);
+			
+			rupFileMagsCache.put(rupMagsFile, rupMags);
+			writtenFiles.add(rupMagsFile);
+		}
 		return mappings;
 	}
 	
@@ -151,31 +188,38 @@ public abstract class TrueMeanRuptureMappings extends AbstractLogicTreeModule {
 	}
 
 	public static TrueMeanRuptureMappings build(LogicTree<?> tree, List<int[]> branchSectMappings,
-			List<int[]> branchRupMappings) {
+			List<int[]> branchRupMappings, List<double[]> branchRupMags) {
 		Preconditions.checkState(tree.size() == branchSectMappings.size());
 		Preconditions.checkState(tree.size() == branchRupMappings.size());
+		Preconditions.checkState(tree.size() == branchRupMags.size());
 		
 		Map<LogicTreeBranch<?>, int[]> branchSectMappingsMap = new HashMap<>(tree.size());
 		Map<LogicTreeBranch<?>, int[]> branchRupMappingsMap = new HashMap<>(tree.size());
+		Map<LogicTreeBranch<?>, double[]> branchRupMagsMap = new HashMap<>(tree.size());
 		for (int i=0; i<tree.size(); i++) {
 			LogicTreeBranch<?> branch = tree.getBranch(i);
 			branchSectMappingsMap.put(branch, branchSectMappings.get(i));
 			branchRupMappingsMap.put(branch, branchRupMappings.get(i));
+			branchRupMagsMap.put(branch, branchRupMags.get(i));
 		}
 		
-		return new InMemory(tree, branchSectMappingsMap, branchRupMappingsMap);
+		return new InMemory(tree, branchSectMappingsMap, branchRupMappingsMap, branchRupMagsMap);
 	}
 	
 	private static class InMemory extends TrueMeanRuptureMappings {
 		
 		private Map<LogicTreeBranch<?>, int[]> branchSectMappings;
 		private Map<LogicTreeBranch<?>, int[]> branchRupMappings;
+		private Map<LogicTreeBranch<?>, double[]> branchRupMags;
 
 		private InMemory(LogicTree<?> logicTree,
-				Map<LogicTreeBranch<?>, int[]> branchSectMappings, Map<LogicTreeBranch<?>, int[]> branchRupMappings) {
+				Map<LogicTreeBranch<?>, int[]> branchSectMappings,
+				Map<LogicTreeBranch<?>, int[]> branchRupMappings,
+				Map<LogicTreeBranch<?>, double[]> branchRupMags) {
 			super(null, null, logicTree);
 			this.branchSectMappings = branchSectMappings;
 			this.branchRupMappings = branchRupMappings;
+			this.branchRupMags = branchRupMags;
 		}
 
 		@Override
@@ -186,6 +230,11 @@ public abstract class TrueMeanRuptureMappings extends AbstractLogicTreeModule {
 		@Override
 		public int[] getRuptureMappings(LogicTreeBranch<?> branch) {
 			return branchRupMappings.get(branch);
+		}
+
+		@Override
+		public double[] getRuptureMags(LogicTreeBranch<?> branch) {
+			return branchRupMags.get(branch);
 		}
 
 	}
@@ -203,16 +252,22 @@ public abstract class TrueMeanRuptureMappings extends AbstractLogicTreeModule {
 		@Override
 		public int[] getSectionMappings(LogicTreeBranch<?> branch) {
 			String sectMappingFile = getBranchFileName(branch, SECT_MAPPING_FILE_NAME, sectMappingLevels);
-			return getCache(sectMappingFile, sectFileMappingsCache);
+			return getIntCache(sectMappingFile, sectFileMappingsCache);
 		}
 
 		@Override
 		public int[] getRuptureMappings(LogicTreeBranch<?> branch) {
 			String rupMappingFile = getBranchFileName(branch, RUP_MAPPING_FILE_NAME, rupMappingLevels);
-			return getCache(rupMappingFile, rupFileMappingsCache);
+			return getIntCache(rupMappingFile, rupFileMappingsCache);
+		}
+
+		@Override
+		public double[] getRuptureMags(LogicTreeBranch<?> branch) {
+			String rupMappingFile = getBranchFileName(branch, RUP_MAG_FILE_NAME, rupMappingLevels);
+			return getDoubleCache(rupMappingFile, rupFileMagsCache);
 		}
 		
-		private synchronized int[] getCache(String fileName, Map<String, int[]> cache) {
+		private synchronized int[] getIntCache(String fileName, Map<String, int[]> cache) {
 			int[] ret = cache.get(fileName);
 			if (ret == null) {
 				// load it
@@ -233,6 +288,33 @@ public abstract class TrueMeanRuptureMappings extends AbstractLogicTreeModule {
 					Preconditions.checkState(index == i,
 							"File out of order, expected index %s at row %s, found %s", i, row, index);
 					ret[index] = csv.getInt(row, 1);
+				}
+				cache.put(fileName, ret);
+			}
+			return ret;
+		}
+		
+		private synchronized double[] getDoubleCache(String fileName, Map<String, double[]> cache) {
+			double[] ret = cache.get(fileName);
+			if (ret == null) {
+				// load it
+				ZipFile zip = getZipFile();
+				ZipEntry entry = zip.getEntry(fileName);
+				Preconditions.checkNotNull(entry, "Entry not found: %s", fileName);
+				
+				CSVFile<String> csv;
+				try {
+					csv = CSVFile.readStream(zip.getInputStream(entry), true);
+				} catch (IOException e) {
+					throw ExceptionUtils.asRuntimeException(e);
+				}
+				ret = new double[csv.getNumRows()-1];
+				for (int i=0; i<ret.length; i++) {
+					int row = i+1;
+					int index = csv.getInt(row, 0);
+					Preconditions.checkState(index == i,
+							"File out of order, expected index %s at row %s, found %s", i, row, index);
+					ret[index] = csv.getDouble(row, 1);
 				}
 				cache.put(fileName, ret);
 			}
