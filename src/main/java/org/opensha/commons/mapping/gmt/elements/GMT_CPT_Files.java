c0fc704848e384b5fd99797ff084dadf8044c5b7
Kevin Milner <kmilner@usgs.gov>
2024-07-17 12:50:13 -0700

    added tableau categorical CPTs

--- src/main/java/org/opensha/commons/mapping/gmt/elements/GMT_CPT_Files.java
+++ src/main/java/org/opensha/commons/mapping/gmt/elements/GMT_CPT_Files.java
@@ -117,7 +117,30 @@ public enum GMT_CPT_Files {
 	 * categorical version of batlow from https://www.fabiocrameri.ch/colourmaps/ and GMT 6
 	 * Grab the first color from each CPT value
 	 */
-	CATEGORICAL_BATLOW_UNIFORM("batlowS.cpt");
+	CATEGORICAL_BATLOW_UNIFORM("batlowS.cpt"),
+	/**
+	 * Categorical colormap from Tableau, commonly used in MatPlotLib (called TAB10), with 10 distinct colors.
+	 * Grab the first color from each CPT value, or call <code>cpt.getColor(index % cpt.size())</code>
+	 */
+	CATEGORICAL_TAB10("tab10.cpt"),
+	/**
+	 * Light version of {@link GMT_CPT_Files#CATEGORICAL_TAB10}. This was created by taking the even numbered values from
+	 * MatPlotLib's TAB20 (the light ones).
+	 */
+	CATEGORICAL_TAB10_LIGHT("tab10_light.cpt"),
+	/**
+	 * Categorical colormap from Tableau, commonly used in matplotlib (called TAB10), with 20 distinct colors.
+	 * <p>
+	 * This is similar to {@link GMT_CPT_Files#CATEGORICAL_TAB10}, except that it has light versions interspersed (see
+	 * {@link GMT_CPT_Files#CATEGORICAL_TAB10_LIGHT}). The dark ones (originals from {@link GMT_CPT_Files#CATEGORICAL_TAB10} 
+	 * are accessible at integer values (e.g., <code>cpt.getColor(index % cpt.size())</code>), and the light colors
+	 * (same as {@link GMT_CPT_Files#CATEGORICAL_TAB10_LIGHT}) are accessible at half fractions (e.g.,
+	 * <code>cpt.getColor(index % cpt.size() + 0.5f)</code>).
+	 * <p>
+	 * If you iterate through CPT values (or access them via index), you will get dark then light of each color before
+	 * moving on to the next color.
+	 */
+	CATEGORICAL_TAB20("tab20.cpt");
 	
 	private String fname;
 	
