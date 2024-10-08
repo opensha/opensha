c4a02a0b870afba580bd8e4b9d475e0ffb02fb32
Kevin Milner <kmilner@usgs.gov>
2024-10-08 09:00:39 -0700

    minor (comments)

--- src/main/java/org/opensha/sha/earthquake/SiteAdaptiveProbEqkSource.java
+++ src/main/java/org/opensha/sha/earthquake/SiteAdaptiveProbEqkSource.java
@@ -3,7 +3,7 @@ package org.opensha.sha.earthquake;
 import org.opensha.commons.data.Site;
 
 /**
- * A {@link ProbEqkSource} that can produce site-dependent variants, likely a grid source that might increase
+ * A {@link ProbEqkSource} that can produce site-dependent variants, e.g., a grid source might increase
  * resolution (supersampling) for nearby sites and use a simpler (faster) representation for far field sites.
  */
 public abstract class SiteAdaptiveProbEqkSource extends ProbEqkSource {
