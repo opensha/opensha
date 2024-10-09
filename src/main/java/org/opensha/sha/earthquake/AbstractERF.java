3d476ef851ea90a0fc35442824cd0fa2e1665666
Kevin Milner <kmilner@usgs.gov>
2024-10-09 10:38:55 -0700

    drawRandomEventSet(site, sourceFilters) now respects SiteAdaptive

--- src/main/java/org/opensha/sha/earthquake/AbstractERF.java
+++ src/main/java/org/opensha/sha/earthquake/AbstractERF.java
@@ -268,6 +268,8 @@ public abstract class AbstractERF implements
 		ArrayList<EqkRupture> rupList = new ArrayList<EqkRupture>();
 		for(int s=0; s<this.getNumSources(); s++) {
 			ProbEqkSource source = getSource(s);
+			if (site != null && source instanceof SiteAdaptiveProbEqkSource)
+				source = ((SiteAdaptiveProbEqkSource)source).getForSite(site);
 			if (doFilter) {
 				boolean skip = false;
 				double dist = source.getMinDistance(site);
