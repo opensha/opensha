9435ab8cb2dc1cb3fcb742a92d59024fbbc1dd5d
Kevin Milner <kmilner@usgs.gov>
2024-10-30 12:39:02 -0700

    cybershake study 24.8 basin depth files

---
+++ src/main/java/org/opensha/commons/data/siteData/servlet/impl/CS_Study24_8_BasinDepthTo2_5_Servlet.java
@@ -0,0 +1,20 @@
+package org.opensha.commons.data.siteData.servlet.impl;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.opensha.commons.data.siteData.SiteData;
+import org.opensha.commons.data.siteData.impl.CS_Study24_8_BasinDepth;
+import org.opensha.commons.data.siteData.servlet.AbstractSiteDataServlet;
+import org.opensha.commons.util.ServerPrefUtils;
+
+public class CS_Study24_8_BasinDepthTo2_5_Servlet extends
+		AbstractSiteDataServlet<Double> {
+	
+	private static final File FILE = new File(ServerPrefUtils.SERVER_PREFS.getTomcatProjectDir(),
+			CS_Study24_8_BasinDepth.DEPTH_2_5_FILE);
+	
+	public CS_Study24_8_BasinDepthTo2_5_Servlet() throws IOException {
+		super(new CS_Study24_8_BasinDepth(SiteData.TYPE_DEPTH_TO_2_5, FILE, false));
+	}
+}
