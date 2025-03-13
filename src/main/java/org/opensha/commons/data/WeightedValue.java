dbe893c3db037b7f762506d2f8b58f775cb52f8d
Akash Bhatthal <akashbhatthal@gmail.com>
2025-03-13 13:28:30 -0700

    Fix IMR names shown in IMR Weights window

--- src/main/java/org/opensha/commons/data/WeightedValue.java
+++ src/main/java/org/opensha/commons/data/WeightedValue.java
@@ -1,5 +1,7 @@
 package org.opensha.commons.data;
 
+import org.opensha.sha.imr.IntensityMeasureRelationship;
+
 public class WeightedValue<E> {
 	public final E value;
 	public final double weight;
@@ -8,5 +10,13 @@ public class WeightedValue<E> {
 		super();
 		this.value = value;
 		this.weight = weight;
+	}
+	
+	@Override
+	public String toString() {
+		if (value instanceof IntensityMeasureRelationship) {
+			return ((IntensityMeasureRelationship) value).getName();
+		}
+		return value.getClass().getName();
 	}
 }
