4e67ca7a8a7defb1247917b1a01ce1e8af8aec70
Kevin Milner <kmilner@usgs.gov>
2025-03-25 14:19:43 -0700

    moved 2008 distance correction to its own class

--- src/main/java/org/opensha/sha/faultSurface/utils/PointSourceDistanceCorrections.java
+++ src/main/java/org/opensha/sha/faultSurface/utils/PointSourceDistanceCorrections.java
@@ -8,6 +8,7 @@ import org.opensha.commons.data.function.EvenlyDiscretizedFunc;
 import org.opensha.sha.earthquake.rupForecastImpl.PointSourceNshm.DistanceCorrection2013;
 import org.opensha.sha.faultSurface.PointSurface;
 import org.opensha.sha.util.NSHMP_Util;
+import org.opensha.sha.util.NSHMP_Util.DistanceCorrection2008;
 
 import com.google.common.base.Preconditions;
 
@@ -54,56 +55,16 @@ public enum PointSourceDistanceCorrections implements Supplier<WeightedList<? ex
 		}
 		
 	}),
-	NSHM_2008("USGS NSHM (2008)", new PointSourceDistanceCorrection() {
-		
-		private EvenlyDiscretizedFunc magBinFunc = new EvenlyDiscretizedFunc(6.05, 26, 0.1);
-
-		@Override
-		public double getCorrectedDistanceJB(double mag, PointSurface surf, double horzDist) {
-			if(mag<=6) {
-				return horzDist;
-			} else if (horzDist == 0d) {
-				return 0d;
-			} else { //if (mag<=7.6) {
-				// NSHMP getMeanRJB is built on the assumption of 0.05 M
-				// centered bins. Non-UCERF erf's often do not make
-				// this assumption and are 0.1 based so we push
-				// the value down to the next closest compatible M
-				
-				// this was Peter's original correction, but it explodes if it's given say 6.449999999999999 (which converts to 6.39999999999999)
-//				double adjMagAlt = ((int) (mag*100) % 10 != 5) ? mag - 0.05 : mag;
-				// this doesn't work either for values like 6.0 and 6.1 (only works when close to an 0.x5)
-//				double adjMag = ((double)Math.round(mag/0.05))*0.05;
-//				if (adjMag > 8.6) adjMag = 8.55;
-				// this works
-				double nearestTenth = Math.round(mag*10)/10d;
-//				System.out.println("Nearest 10th to "+mag+" is "+nearestTenth);
-				if ((float)nearestTenth > 6f && (float)nearestTenth == (float)mag)
-					// we're right at a 10th and want it to always round down
-					// e.g., we don't want 6.449999999999999 to round down, but 6.450000000000001 to round up
-					// so subtract a tiny bit from the nearest tenth to force it to always round down
-					mag = nearestTenth - 0.0001;
-				double adjMag = magBinFunc.getX(magBinFunc.getClosestXIndex(mag));
-//				if(adjMagAlt != adjMag)
-//					System.out.println("mag,adj,alt:\t"+mag+"\t"+adjMag+"\t"+adjMagAlt);
-//				System.out.println("\tadjMag="+(float)adjMag);
-				return NSHMP_Util.getMeanRJB(adjMag, horzDist);
-			}
-		}
-		
-		@Override
-		public String toString() {
-			return PointSourceDistanceCorrections.NSHM_2008.name;
-		}
-		
-	}),
+	NSHM_2008("USGS NSHM (2008)", new DistanceCorrection2008()),
 	NSHM_2013("USGS NSHM (2013)", new DistanceCorrection2013()),
 	ANALYTICAL_MEDIAN("Analytical Median (centered)",
 			// NaN here indicates to use mean and not a fractile
 			new AnalyticalPointSourceDistanceCorrection(Double.NaN, false, false)),
 	ANALYTICAL_FIVE_POINT("Analytical 5-Point (centered)",
 //			AnalyticalPointSourceDistanceCorrection.getEvenlyWeightedFractiles(5, false, false)),
-			AnalyticalPointSourceDistanceCorrection.getImportanceSampledFractiles(new double[] {0d, 0.05, 0.2, 0.5, 0.8, 1d}, false, false));
+			AnalyticalPointSourceDistanceCorrection.getImportanceSampledFractiles(new double[] {0d, 0.05, 0.2, 0.5, 0.8, 1d}, false, false)),
+	ANALYTICAL_TWENTY_POINT("Analytical 20-Point (centered)",
+			AnalyticalPointSourceDistanceCorrection.getEvenlyWeightedFractiles(20, false, false));
 	
 	// TODO: decide on default
 	public static final PointSourceDistanceCorrections DEFAULT = NSHM_2013;
