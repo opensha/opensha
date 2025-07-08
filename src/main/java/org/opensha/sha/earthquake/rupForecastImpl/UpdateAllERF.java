9783ab17392a8b304c6867be3ca0033364f727c1
Akash Bhatthal <akashbhatthal@gmail.com>
2025-07-08 15:23:25 -0700

    Read JSON instead of using hardcoded model names

--- src/main/java/org/opensha/sha/earthquake/rupForecastImpl/UpdateAllERF.java
+++ src/main/java/org/opensha/sha/earthquake/rupForecastImpl/UpdateAllERF.java
@@ -1,11 +1,17 @@
 package org.opensha.sha.earthquake.rupForecastImpl;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.net.URI;
 import java.util.concurrent.ExecutionException;
 
 import org.opensha.sha.earthquake.rupForecastImpl.nshm23.util.NSHM23_Downloader;
 import org.scec.getfile.GetFile;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  * Test that we're able to download all ERF data using GetFile.
@@ -13,49 +19,49 @@ import org.scec.getfile.GetFile;
  * This should only be run occassionally as updating all files may be expensive.
  */
 public class UpdateAllERF {
-		private static final String[] UCERF3_DAT = {
-			"branch_avgs_combined",
-			"cached_dep100.0_depMean_rakeMean",
-			"cached_FM3_1_dep100.0_depMean_rakeMean",
-			"cached_FM3_2_dep100.0_depMean_rakeMean",
-			"FM3_1_ABM_Shaw09Mod_DsrUni_CharConst_M5Rate7.9_MMaxOff7.6_NoFix_SpatSeisU3",
-			"FM3_1_branch_averaged_full_modules",
-			"FM3_1_branch_averaged_with_logic_tree",
-			"FM3_1_branch_averaged",
-			"FM3_1_GEOL_Shaw09Mod_DsrUni_CharConst_M5Rate7.9_MMaxOff7.6_NoFix_SpatSeisU3",
-			"FM3_1_mean_ucerf3_sol",
-			"FM3_1_NEOK_Shaw09Mod_DsrUni_CharConst_M5Rate7.9_MMaxOff7.6_NoFix_SpatSeisU3",
-			"FM3_1_SpatSeisU2_branch_averaged_full_modules",
-			"FM3_1_SpatSeisU2_branch_averaged",
-			"FM3_1_SpatSeisU3_branch_averaged_full_modules",
-			"FM3_1_SpatSeisU3_branch_averaged",
-			"FM3_1_ZENGBB_Shaw09Mod_DsrTap_CharConst_M5Rate7.9_MMaxOff7.6_NoFix_SpatSeisU3",
-			"FM3_1_ZENGBB_Shaw09Mod_DsrUni_CharConst_M5Rate7.9_MMaxOff7.6_NoFix_SpatSeisU3",
-			"FM3_2_ABM_Shaw09Mod_DsrUni_CharConst_M5Rate7.9_MMaxOff7.6_NoFix_SpatSeisU3",
-			"FM3_2_branch_averaged_full_modules",
-			"FM3_2_branch_averaged_with_logic_tree",
-			"FM3_2_branch_averaged",
-			"FM3_2_GEOL_Shaw09Mod_DsrUni_CharConst_M5Rate7.9_MMaxOff7.6_NoFix_SpatSeisU3",
-			"FM3_2_mean_ucerf3_sol",
-			"FM3_2_NEOK_Shaw09Mod_DsrUni_CharConst_M5Rate7.9_MMaxOff7.6_NoFix_SpatSeisU3",
-			"FM3_2_SpatSeisU2_branch_averaged_full_modules",
-			"FM3_2_SpatSeisU2_branch_averaged",
-			"FM3_2_SpatSeisU3_branch_averaged_full_modules",
-			"FM3_2_SpatSeisU3_branch_averaged",
-			"FM3_2_ZENGBB_Shaw09Mod_DsrTap_CharConst_M5Rate7.9_MMaxOff7.6_NoFix_SpatSeisU3",
-			"FM3_2_ZENGBB_Shaw09Mod_DsrUni_CharConst_M5Rate7.9_MMaxOff7.6_NoFix_SpatSeisU3",
-			"full_logic_tree_with_gridded",
-			"full_logic_tree",
-			"mean_ucerf3_sol_with_mappings",
-			"mean_ucerf3_sol",
-			"rake_basis",
-			"full_ucerf3_compound_sol",
-		};
+	/**
+	 * Returns a list of all key entries from the JSON
+	 * @param csvFile The metadata file listing all JSON entries
+	 * @return
+	 */
+	private static final String[] getModels(File jsonFile) {
+		try (Reader reader = new FileReader(jsonFile)) {
+			JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+			String[] keys = jsonObject.keySet().toArray(new String[0]);
+			return keys;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Download all ERF data for a given ERF
+	 * @param erfName	 Name of ERF (i.e. ucerf3, nshm23)
+	 * @param downloader GetFile instance to download specific ERF
+	 * @return true if downloaded successfully
+	 */
+	public static boolean updateERF(String erfName, GetFile downloader) {
+		try {
+			System.out.println("Updating all ERF data for " + erfName);
+			downloader.updateAll().get();
+			for (String dat : getModels(
+					new File(System.getProperty("user.home"), ".opensha/"+erfName+"/"+erfName+".json"))) {
+				File model = new File(System.getProperty("user.home"), ".opensha/"+erfName+"/" + dat + ".zip");
+				System.out.println("Checking if " + model.toString() + " exists...");
+				if (!model.exists()) {
+					System.out.println(model.toString() + " is missing!");
+					return false;
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
 		
-		private static final String[] NSHM23_DAT = {
-			"WUS_branch_averaged_gridded_simplified",
-		};
-
 	public static void main(String[] args) {
 		final GetFile UCERF3_DOWNLOADER = new GetFile(
 				/*name=*/"UCERF3",
@@ -64,23 +70,16 @@ public class UpdateAllERF {
 				/*serverMetaURI=*/URI.create(
 						"https://g-c662a6.a78b8.36fe.data.globus.org/getfile/ucerf3/ucerf3.json"),
 				/*showProgress=*/false);
+		final GetFile NSHM23_DOWNLOADER =
+				new NSHM23_Downloader(/*showProgress=*/false);
 		
-		try {
-			UCERF3_DOWNLOADER.updateAll().get();
-			for (String dat : UCERF3_DAT) {
-				File model = new File(System.getProperty("user.home"), ".opensha/ucerf3/" + dat);
-				assert(model.exists());
-			}
-			
-		new NSHM23_Downloader(/*showProgress=*/false).updateAll().get();
-			for (String dat : NSHM23_DAT) {
-				File model = new File(System.getProperty("user.home"), ".opensha/nshm23/" + dat);
-				assert(model.exists());
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
+		boolean allUpdated = updateERF("ucerf3", UCERF3_DOWNLOADER) &&
+							 updateERF("nshm23", NSHM23_DOWNLOADER);
+		
+		if (allUpdated) {
+			System.out.println("All ERFs were updated successfully!");
+		} else {
+			System.out.println("Failed to update all ERFs!");
 		}
 	}
 
