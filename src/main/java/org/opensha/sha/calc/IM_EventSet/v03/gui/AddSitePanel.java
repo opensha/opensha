34412774c862c13a13e9e06fe27dae6b83c1a3b5
Akash Bhatthal <akashbhatthal@gmail.com>
2025-07-31 01:52:44 -0700

    Create help panel inside AddSitePanel

--- src/main/java/org/opensha/sha/calc/IM_EventSet/v03/gui/AddSitePanel.java
+++ src/main/java/org/opensha/sha/calc/IM_EventSet/v03/gui/AddSitePanel.java
@@ -6,9 +6,14 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 import org.opensha.commons.data.siteData.SiteDataValue;
+import org.opensha.commons.data.siteData.impl.WillsMap2000;
 import org.opensha.commons.geo.Location;
+import org.opensha.commons.gui.LabeledBoxPanel;
 import org.opensha.commons.param.ParameterList;
 import org.opensha.commons.param.editor.impl.ParameterListEditor;
 import org.opensha.commons.param.impl.DoubleParameter;
@@ -23,14 +28,37 @@ public class AddSitePanel extends JPanel {
 	public AddSitePanel() {
 		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		
+
 		ParameterList paramList = new ParameterList();
 		paramList.addParameter(latParam);
 		paramList.addParameter(lonParam);
 		ParameterListEditor paramEdit = new ParameterListEditor(paramList);
 		paramEdit.setTitle("New Site Location");
-		paramEdit.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 10));
+//		paramEdit.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+		
+		LabeledBoxPanel help = new LabeledBoxPanel();
+		help.setTitle("Help");
+		JTextArea helpText = new JTextArea(5, 20);
+		helpText.setText(
+				"Specify location and data value, then click \"Add\".\n"
+				+ "After adding all values, click \"OK\" to continue.\n\n"
+				+ "Default Location (34.0, -118.0) = Los Angeles\n\n"
+				+ "Vs30, Depth to Vs : Numeric\n\n"
+				+ "Wills Class : " + WillsMap2000.wills_vs30_map.keySet());
+		help.add(helpText);
 
-		this.add(paramEdit, true);
+		Border line = BorderFactory.createLineBorder(help.getBorderColor());
+		Border padding = BorderFactory.createEmptyBorder(10, 0, 0, 0);
+		help.setBorder(BorderFactory.createCompoundBorder(padding, line));
+
+		JPanel leftCol = new JPanel();
+		leftCol.setLayout(new BoxLayout(leftCol, BoxLayout.Y_AXIS));
+		leftCol.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 10));
+
+		leftCol.add(paramEdit);
+		leftCol.add(help);
+		
+		this.add(leftCol);
 		
 		adder = new AddMultipleSiteDataPanel();
 		this.add(adder);
