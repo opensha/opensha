78a2b02076adac2bb2177b1805f57861256b975a
voj <o.jurgens@gns.cri.nz>
2026-03-09 13:01:21 +1300

    indexes

---
+++ src/test/java/org/opensha/sha/earthquake/faultSysSolution/ruptures/multiRupture/RuptureProximityLookupSuite.java
@@ -0,0 +1,8 @@
+package org.opensha.sha.earthquake.faultSysSolution.ruptures.multiRupture;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({RuptureProximityLookupTest.class})
+public class RuptureProximityLookupSuite {}
