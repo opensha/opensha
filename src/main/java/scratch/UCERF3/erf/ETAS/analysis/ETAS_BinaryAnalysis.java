4886dac3c7c4f5baa4e44e7ece3d4278541654ba
Akash Bhatthal <akashbhatthal@gmail.com>
2026-06-23 14:02:45 -0700

    Improve runtime by only iterating over catalogs once and only collecting catalog metadata for primary rup.

--- src/main/java/scratch/UCERF3/erf/ETAS/analysis/ETAS_BinaryAnalysis.java
+++ src/main/java/scratch/UCERF3/erf/ETAS/analysis/ETAS_BinaryAnalysis.java
@@ -1,11 +1,9 @@
 package scratch.UCERF3.erf.ETAS.analysis;
 
 import org.apache.commons.cli.*;
-import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.math3.util.CombinatoricsUtils;
-import org.apache.commons.math3.util.Precision;
+import org.apache.commons.io.file.PathUtils;
 
-import org.opensha.sha.earthquake.EqkRupture;
 import org.opensha.sha.earthquake.faultSysSolution.FaultSystemRupSet;
 import org.opensha.sha.earthquake.faultSysSolution.FaultSystemSolution;
 import org.opensha.sha.earthquake.faultSysSolution.util.FaultSectionUtils;
@@ -13,8 +11,9 @@ import org.opensha.sha.faultSurface.FaultSection;
 import scratch.UCERF3.erf.ETAS.ETAS_EqkRupture;
 import scratch.UCERF3.erf.ETAS.ETAS_SimulationMetadata;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -42,11 +41,8 @@ public class ETAS_BinaryAnalysis {
     private final RuptureSearcher rupSearch;
     private final FaultSystemSolution fss; // Fault System Solution as defined by the ZIP file
 
-
     // Smallest magnitude to consider from binary catalogs
     private static final double MIN_MAG = 2.5;
-    // Debug messages
-    private static final boolean D = true;
 
     /**
      * Constructor executes workflow for parsing binary and generating markdown
@@ -54,31 +50,45 @@ public class ETAS_BinaryAnalysis {
      * @throws IOException
      */
     public ETAS_BinaryAnalysis(CommandLine cmd) throws IOException {
+        System.out.println("Parsing ETAS results binary...");
         this.parser = new BinaryParser(new File(cmd.getOptionValue("bin")));
+        System.out.println("Loading fault system solution...");
         this.fss = FaultSystemSolution.load(new File(cmd.getOptionValue("fss")));
 
         // Generate report for the scenario aftershock
         int aftershock = Integer.parseInt(cmd.getOptionValue("rup"));
 
-        MarkdownGenerator markdownGen = new MarkdownGenerator();
-        System.out.println("# Primary Aftershock (FSS=" + aftershock + ")");
-        markdownGen.generateReport(aftershock);
+        try (MarkdownGenerator markdownGen = new MarkdownGenerator(Path.of(cmd.getOptionValue("out")))) {
+            System.out.println("Building ETAS Binary analysis markdown report...");
 
-        // How often any rupture (not just our exact scenario) was triggered on the Hollywood fault, Raymond fault, or both together
-        final List<String> parentSections = List.of("Hollywood", "Raymond");
-        final List<Integer> parentSectionIDs = parentSections.stream()
-                            .map(s -> FaultSectionUtils.findParentSectionID(fss.getRupSet().getFaultSectionDataList(), s))
-                            .toList();
-        this.rupSearch = new RuptureSearcher(parentSectionIDs);
+            markdownGen.generateReport(aftershock);
+            System.out.println("Primary aftershock reported");
 
-        // TODO: We need to first filter the candidate rupture sets with what actually occurs in the binary
-        // Currently it's just printing all from the FSS and reporting all with 0 occurrences
-        List<String> parentSectionRupHeaders = rupSearch.getIntersectionHeaders();
-        List<List<Integer>> parentSectionIntersection = rupSearch.getCandidateRuptureSets();
-        for (int i = 0; i < parentSectionRupHeaders.size(); i++) {
-            String header = parentSectionRupHeaders.get(i);
-            System.out.println(header);
-            parentSectionIntersection.get(i).forEach(markdownGen::generateReport);
+            // How often any rupture (not just our exact scenario) was triggered on the Hollywood fault, Raymond fault, or both together
+            final List<String> parentSections = List.of("Hollywood", "Raymond");
+            final List<Integer> parentSectionIDs = parentSections.stream()
+                    .map(s -> FaultSectionUtils.findParentSectionID(fss.getRupSet().getFaultSectionDataList(), s))
+                    .toList();
+            this.rupSearch = new RuptureSearcher(parentSectionIDs);
+
+            List<String> parentSectionRupHeaders = rupSearch.getIntersectionHeaders();
+            List<List<Integer>> parentSectionIntersection = rupSearch.getCandidateRuptureSets();
+            parser.countAftershocks(parentSectionIntersection
+                    .stream()
+                    .flatMap(List::stream)
+                    .collect(Collectors.toSet()));
+            for (int i = 0; i < parentSectionRupHeaders.size(); i++) {
+                List<Integer> rupSet = parentSectionIntersection.get(i);
+                int aftershocksCount = 0;
+                String header = parentSectionRupHeaders.get(i);
+                    for (int rupIdx : rupSet) {
+                        aftershocksCount += parser.aftershocksCount.get(rupIdx);
+                    }
+                    int totalCount = parser.getAllCatalogsCount();
+                    markdownGen.write(String.format(
+                            "%s\nFound %d aftershocks over these parent sections across %d simulated catalogs (%.2f%%).",
+                            header, aftershocksCount, totalCount, 100. * aftershocksCount / totalCount));
+            }
         }
     }
 
@@ -91,10 +101,7 @@ public class ETAS_BinaryAnalysis {
         RuptureSearcher(List<Integer> parentSectionIDs) {
             // Use `FaultSectionUtils.findParentSectionID` to find IDs from section names
             this.parentSectionIDs = parentSectionIDs;
-            if (D) {
-                System.out.print("Searching for all ruptures in the Fault System Solution on all combinations of the following parent section IDs: ");
-                System.out.println(parentSectionIDs);
-            }
+            System.out.println("Searching for all ruptures in the Fault System Solution on all combinations of the following parent section IDs: " + parentSectionIDs);
         }
 
         /**
@@ -103,31 +110,33 @@ public class ETAS_BinaryAnalysis {
          * @return List of List fss indices for ruptures
          */
         public List<List<Integer>> getCandidateRuptureSets() {
+            // TODO: Add summary stats for how many ruptures found in binary occur on Hollywood, Raymond, and both together
+            // TODO: We don't care to know how many of each specific rupture occurs, except for the primary aftershock rupture
             List<List<Integer>> results = new ArrayList<>();
             FaultSystemRupSet rupSet = fss.getRupSet();
             // Add each rupture that matches one of the valid parent section combinations
             for (List<Integer> comb : getParentSectionCombinations()) {
                 // Get all ruptures that occur in all parent sections in the given combination
-                List<Integer> intersection = rupSet.getParentSectionsForRup(comb.get(0));
+                Set<Integer> intersection = new HashSet<>(rupSet.getRupturesForParentSection(comb.get(0)));
                 for (int i = 1; i < comb.size(); i++) {
-                    Set<Integer> currentSet = new HashSet<>(rupSet.getParentSectionsForRup(comb.get(i)));
+                    Set<Integer> currentSet = new HashSet<>(rupSet.getRupturesForParentSection(comb.get(i)));
                     intersection.retainAll(currentSet);
                 }
-                results.add(intersection);
+                results.add(new ArrayList<>(intersection));
             }
             return results;
         }
 
         /**
          * Gets a header name for each parent section combination
-         * @return parent section name for first matching FaultSection, and "None" if no matches
+         * @return parent section name for first matching FaultSection, and ID if no matches
          */
         public List<String> getIntersectionHeaders() {
             // We need to find the name for each parent section ID and build headers accordingly
             List<String> headers = new ArrayList<>();
             List<List<Integer>> combinations = getParentSectionCombinations();
             for (List<Integer> comb : combinations) {
-                StringBuilder header = new StringBuilder("# Ruptures on ");
+                StringBuilder header = new StringBuilder("## Ruptures on ");
                 header.append(comb.stream()
                         .map(this::getParentSectionName)
                         .collect(Collectors.joining(", ")));
@@ -147,7 +156,7 @@ public class ETAS_BinaryAnalysis {
                    return sect.getParentSectionName();
                 }
             }
-            return "None";
+            return String.valueOf(parentID);
         }
 
         /**
@@ -183,20 +192,36 @@ public class ETAS_BinaryAnalysis {
     private class BinaryParser {
         private final File binFile; // UCERF3-ETAS results binary
         // Map rupture ID to list of catalogs matching scenario
-        private final Map<Integer, List<ETAS_Catalog>> catalogMatches = new HashMap<>();
+        public final Map<Integer, List<ETAS_Catalog>> catalogMatches = new HashMap<>();
+        // Map MIN_MAG to catalog counts
+        private final Map<Double, Integer> significantCatalogsCount = new HashMap<>();
+        // Total simulated catalogs in provided binary
+        private final int allCatalogsCount;
+        // How many of a given aftershock (FSS idx) are found across all catalogs (≥ MIN_MAG)
+        public final Map<Integer, Integer> aftershocksCount = new HashMap<>();
+
 
         BinaryParser(File binFile) {
             this.binFile = binFile;
+            this.allCatalogsCount = getBinaryCatalogsIterable(binFile, 0).getNumCatalogs();
        }
 
         /**
-         * How many simulated catalogs match the aftershock scenario description?
-         * @param fssIdx FSS index of aftershock rupture
+         * How many simulated catalogs match each aftershock scenario description?
+         * @param fssIdxs Set of FSS index for each aftershock rupture
          * @return
          */
-        public int getAftershockCount(int fssIdx) {
-            if (!catalogMatches.containsKey(fssIdx)) collectCatalogsMatching(fssIdx);
-            return catalogMatches.get(fssIdx).size();
+        public void countAftershocks(Set<Integer> fssIdxs) throws IOException {
+            fssIdxs.forEach(id -> aftershocksCount.put(id, 0));
+
+            for (ETAS_Catalog cat : getBinaryCatalogsIterable(binFile, MIN_MAG)) {
+                for (ETAS_EqkRupture rup : cat) {
+                    int id = rup.getFSSIndex();
+                    if (fssIdxs.contains(id)) {
+                        aftershocksCount.merge(id, 1, Integer::sum);
+                    }
+                }
+            }
         }
 
         /**
@@ -204,7 +229,7 @@ public class ETAS_BinaryAnalysis {
          * @return
          */
         public int getAllCatalogsCount() {
-            return getBinaryCatalogsIterable(binFile, 0).getNumCatalogs();
+            return allCatalogsCount;
         }
 
         /**
@@ -212,8 +237,15 @@ public class ETAS_BinaryAnalysis {
          * @return
          */
         public int getSignificantCatalogsCount() {
-            return (int)StreamSupport.stream(getBinaryCatalogsIterable(binFile, MIN_MAG).spliterator(), false)
-                    .filter(catalog -> !catalog.isEmpty()).count();
+            if (significantCatalogsCount.containsKey(MIN_MAG)) {
+                return significantCatalogsCount.get(MIN_MAG);
+            }
+            int count = (int)StreamSupport
+                    .stream(getBinaryCatalogsIterable(binFile, MIN_MAG).spliterator(), false)
+                    .filter(catalog -> !catalog.isEmpty())
+                    .count();
+            significantCatalogsCount.put(MIN_MAG, count);
+            return count;
         }
 
         /**
@@ -227,61 +259,81 @@ public class ETAS_BinaryAnalysis {
          * @param fssIdx FSS index of aftershock rupture
          * @return
          */
-        private void collectCatalogsMatching(int fssIdx) {
+        public void collectCatalogsMatching(int fssIdx) {
             catalogMatches.put(fssIdx, new ArrayList<>());
-            for (ETAS_Catalog catalog : getBinaryCatalogsIterable(binFile, MIN_MAG)) {
-                if (catalogHasAftershock(catalog, fssIdx)) {
-                    catalogMatches.get(fssIdx).add(catalog);
+            for (ETAS_Catalog cat : getBinaryCatalogsIterable(binFile, MIN_MAG)) {
+                for (ETAS_EqkRupture rup : cat) {
+                    if (fssIdx == rup.getFSSIndex()) {
+                    catalogMatches.get(fssIdx).add(cat);
+                    }
                 }
             }
         }
-
-        /**
-         * A simulated catalog is considered to have an aftershock if there
-         * is at least one match for the FSS index.
-         * @param catalog
-         * @param fssIdx
-         * @return
-         */
-        private boolean catalogHasAftershock(ETAS_Catalog catalog, int fssIdx) {
-            for (ETAS_EqkRupture rupture : catalog) {
-//                if (rupture.getFSSIndex() != -1)
-//                    System.out.println("id=" + rupture.getID() + ", fss="+rupture.getFSSIndex());
-                if (rupture.getFSSIndex() == fssIdx) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
     }
 
     /**
      * Nested helper class for markdown generation
      */
-    private class MarkdownGenerator {
+    private class MarkdownGenerator implements AutoCloseable {
+
+        private final Path mdFile;
+        private final Path plotsDir;
+        private final BufferedWriter writer;
+        final Object fileLock = new Object(); // Shared lock across all futures
+
         /**
-         * Generates a report for the `fssIndex` rupture using the binary parser
+         * Generates a new markdown report and plots at the given output directory.
+         * This will overwrite any existing reports.
+         * @param outputDir
+         */
+        MarkdownGenerator(Path outputDir) throws IOException {
+            this.mdFile = outputDir.resolve("report.md");
+            PathUtils.createParentDirectories(mdFile);
+            Files.deleteIfExists(mdFile);
+            Files.createFile(mdFile);
+            this.plotsDir = outputDir.resolve("plots");
+            PathUtils.createParentDirectories(plotsDir);
+            if (Files.exists(plotsDir)) {
+                PathUtils.deleteDirectory(plotsDir);
+            }
+            Files.createDirectories(plotsDir);
+
+            this.writer = Files.newBufferedWriter(mdFile);
+        }
+
+        /**
+         * Writes a single line to the existing report.
+         * @param line
+         */
+        public void write(String line) throws IOException {
+            synchronized (fileLock) {
+                writer.write(line);
+                writer.newLine();
+            }
+        }
+
+        /**
+         * Generates a report for the `fssIndex` rupture using the binary parser.
+         * Appends to the existing markdown output.
          * @param fssIndex
          */
-        public void generateReport(int fssIndex) {
-            // For now this just reports statistics to stdout
-            System.out.printf("## Rupture: " + fssIndex);
+        public void generateReport(int fssIndex) throws IOException {
+            write("# ETAS Binary Analysis Report");
             FaultSystemRupSet rupSet = fss.getRupSet();
             double mag = rupSet.getMagForRup(fssIndex);
-            System.out.println(", M"+mag);
+            write(String.format("Primary Aftershock (FSS=%d), M%f", fssIndex, mag));
             int totalCount = parser.getAllCatalogsCount();
             int sigCount = parser.getSignificantCatalogsCount();
-            int aftershocksCount = parser.getAftershockCount(fssIndex);
-            System.out.println("### Counts");
-            System.out.println("Total number of simulations: " + totalCount);
-            System.out.println("Total number of simulations M≥"+MIN_MAG+": " + sigCount);
-            System.out.println("Total number of simulations with aftershock occurrence: " + aftershocksCount);
-            System.out.println("### Probabilities");
-            System.out.printf("Total: %.2E, Sig: %.2E\n", (double)aftershocksCount/totalCount, (double)aftershocksCount/sigCount);
-            System.out.println("### Catalog Matches");
+            parser.countAftershocks(Set.of(fssIndex));
+            int aftershocksCount = parser.aftershocksCount.get(fssIndex);
+            write("Total number of simulations: " + totalCount);
+            write("Total number of simulations M≥"+MIN_MAG+": " + sigCount);
+            write("Total number of simulations with aftershock occurrence: " + aftershocksCount);
+            write(String.format("Total: %.2f%%, Sig: %.2f%%", 100. * aftershocksCount/totalCount, 100. * aftershocksCount/sigCount));
+            write("## Catalog Matches");
+            parser.collectCatalogsMatching(fssIndex);
             for (ETAS_Catalog catalog : parser.catalogMatches.get(fssIndex)) {
-                System.out.println(getCatalogMeta(catalog));
+                write(getCatalogMeta(catalog));
             }
         }
 
@@ -293,7 +345,7 @@ public class ETAS_BinaryAnalysis {
          * @return
          */
         private String getCatalogMeta(ETAS_Catalog catalog) {
-            // TODO: We want the timing of the rupture from start of the simulated catalog (e.g., within a day)
+            // TODO: We want the timing of the first rupture from start of the simulated catalog (e.g., within a day)
             StringBuilder output = new StringBuilder("### Catalog Index: ");
             ETAS_SimulationMetadata meta = catalog.getSimulationMetadata();
             output.append(meta.catalogIndex);
@@ -301,6 +353,7 @@ public class ETAS_BinaryAnalysis {
             output.append("Simulation Start Time: ");
             output.append(getDate(meta.simulationStartTime));
             output.append("\n");
+            // TODO: Why is the end time not impacted?
             output.append("Simulation End Time: ");
             output.append(getDate(meta.simulationEndTime));
             output.append("\n");
@@ -323,6 +376,13 @@ public class ETAS_BinaryAnalysis {
             // 3. Format into a human-readable string
             return formatter.format(instant);
 
+        }
+
+        @Override
+        public void close() throws IOException {
+            if (writer != null) {
+                writer.close();
+            }
         }
 
         // TODO: Generate plots
@@ -365,6 +425,14 @@ public class ETAS_BinaryAnalysis {
                 .argName("rupture-id")
                 .build());
 
+        options.addOption(Option.builder("o")
+                .longOpt("out")
+                .desc("Output Directory")
+                .required(true)
+                .hasArg()
+                .argName("dir-path")
+                .build());
+
 
         return options;
     }
@@ -388,7 +456,8 @@ public class ETAS_BinaryAnalysis {
             args = new String[]{
                     "--fss", "/Users/bhatthal/git/ucerf3-etas-launcher/inputs/2013_05_10-ucerf3p3-production-10runs_COMPOUND_SOL_FM3_1_SpatSeisU3_MEAN_BRANCH_AVG_SOL.zip",
                     "--bin", "/Users/bhatthal/Downloads/2026_05_27-FSS_Rupture_201887_M7p8_Start_2026_10_15_1_yr_kCOV_1p5_MaxPtSrcM_6/results_m5_preserve_chain.bin",
-                    "--rup", "218331"
+                    "--rup", "218331",
+                    "--out", "/Users/bhatthal/Desktop/etas-bin-analysis"
             };
         }
         System.setProperty("java.awt.headless", "true");
