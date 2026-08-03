d7ab33e316b493dd0374864c3bec5e0e69dbb770
Kevin Milner <kmilner@usgs.gov>
2026-08-03 15:51:33 -0700

    added metadata display

--- src/main/java/org/opensha/sha/earthquake/rupForecastImpl/nshm23/erf/NSHM23_BranchAveragedERF.java
+++ src/main/java/org/opensha/sha/earthquake/rupForecastImpl/nshm23/erf/NSHM23_BranchAveragedERF.java
@@ -8,18 +8,19 @@ import javax.swing.JOptionPane;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.opensha.commons.param.impl.EnumParameter;
+import org.opensha.commons.param.impl.MultilineStringParameter;
 import org.opensha.sha.earthquake.faultSysSolution.FaultSystemSolution;
 import org.opensha.sha.earthquake.faultSysSolution.erf.BaseFaultSystemSolutionERF;
 import org.opensha.sha.earthquake.rupForecastImpl.nshm23.util.NSHM23_Downloader;
 import org.opensha.sha.util.TectonicRegionType;
 
 /**
- * USGS 2023 NSHM ERF, Western U.S., Branch Averaged ERF
+ * USGS 2023 NSHM Branch Averaged ERF. This ERF supports multiple model versions, which can represent revisions to the
+ * model or different regions (e.g., WUS vs full CONUS, Alaska, Cascadia).
  */
 public class NSHM23_BranchAveragedERF extends BaseFaultSystemSolutionERF {
 	
-	// TODO updated UID
-	private static final long serialVersionUID = 277613161331416141L;
+	private static final long serialVersionUID = 2L;
 	
 	// TODO updated default to R2 when file is posted
 	public static final ModelVersions MODEL_DEFAULT = ModelVersions.WUS_R1;
@@ -32,24 +33,31 @@ public class NSHM23_BranchAveragedERF extends BaseFaultSystemSolutionERF {
 	private EnumParameter<ModelVersions> modelParam;
 	private ModelVersions model;
 	
+	private static final String METADATA_PARAM_NAME = "Model Information";
+	private MultilineStringParameter modelInfoParam;
+	
 	public enum ModelVersions {
-		WUS_R2("WUS-Crustal R2 (excl. Cascadia)", "WUS_branch_averaged_gridded_simplified_R2",
+		WUS_R2("WUS-Crustal R2 (excluding Cascadia)", "WUS_branch_averaged_gridded_simplified_R2",
 				"""
-				Western U.S. crustal sources west of lon=-105, including active crustal fault sources and both active and \
-				stable gridded seismicity sources. Cascadia subduction interface and intraslab seismicity are excluded.
+				Revision 2 of western U.S. crustal sources (longitude<-105), including active crustal fault sources and \
+				both active and stable gridded seismicity sources. Cascadia subduction interface and intraslab seismicity \
+				are excluded.
 				
 				This is revision 2 of the model described in Powers et al. (2026), primarily updated to reclassify stable \
 				continental gridded seismicity in the Intermountain West as 2/3 active crustal and 1/3 stable continental. \
 				This version also contains minor updates to the rounding of crustal rupture rakes, and updates stable \
-				continental gridded seismicity rupture properties to match thosed used in NSHMP-Haz.""",
+				continental gridded seismicity rupture properties to match those used in NSHMP-Haz.""",
 				EnumSet.of(TectonicRegionType.ACTIVE_SHALLOW, TectonicRegionType.STABLE_SHALLOW)),
 		
-		WUS_R1("WUS-Crustal R1 (excl. Cascadia)", "WUS_branch_averaged_gridded_simplified",
+		WUS_R1("WUS-Crustal R1 (excluding Cascadia)", "WUS_branch_averaged_gridded_simplified",
 				"""
-				Western U.S. crustal sources west of lon=-105, including active crustal fault sources and both active and \
+				Western U.S. crustal sources (longitude<-105), including active crustal fault sources and both active and \
 				stable gridded seismicity sources. Cascadia subduction interface and intraslab seismicity are excluded.
 				
-				This is the original published NSHM23 model.""",
+				This is the original published NSHM23 model as implemented and released in OpenSHA (revision 1). Note \
+				that a subsequent revision (R2) includes additional tweaks to better align with the official USGS \
+				implementation, including rounding of rake values (which can affect rupture fault-style assignment) \
+				and updated stable continental gridded seismicity rupture properties (Ztor, Zhyp).""",
 				EnumSet.of(TectonicRegionType.ACTIVE_SHALLOW, TectonicRegionType.STABLE_SHALLOW));
 		
 		public final String displayName;
@@ -62,6 +70,11 @@ public class NSHM23_BranchAveragedERF extends BaseFaultSystemSolutionERF {
 			this.prefix = prefix;
 			this.metadata = metadata;
 			this.trts = trts;
+		}
+
+		@Override
+		public String toString() {
+			return displayName;
 		}
 	}
 	
@@ -96,6 +109,12 @@ public class NSHM23_BranchAveragedERF extends BaseFaultSystemSolutionERF {
 		
 		modelParam = new EnumParameter<>(MODEL_PARAM_NAME, EnumSet.allOf(ModelVersions.class), model, null);
 		modelParam.addParameterChangeListener((e) -> { modelChanged();});
+		
+		modelInfoParam = new MultilineStringParameter(METADATA_PARAM_NAME);
+		modelInfoParam.setTextEditable(false); // this is display-only metadata
+		modelInfoParam.setRows(7); // make it a little longer in the apps
+		modelInfoParam.setIncludedInMetadata(false); // don't include in the parameter value metadata strings
+		
 		modelChanged();
 		
 		initParams();
@@ -107,6 +126,9 @@ public class NSHM23_BranchAveragedERF extends BaseFaultSystemSolutionERF {
 		// set the TRTs explicitly so that getIncludedTectonicRegionTypes() knows them without updating the forecast first
 		this.erfTRTs = model.trts;
 		fireTRTChangeEvent();
+		
+		modelInfoParam.setValue(model.metadata);
+		modelInfoParam.refreshEditor();
 	}
 	
 	/**
@@ -118,7 +140,7 @@ public class NSHM23_BranchAveragedERF extends BaseFaultSystemSolutionERF {
 		if (adjustableParams.containsParameter(FILE_PARAM_NAME))
 			adjustableParams.removeParameter(fileParam);
 		adjustableParams.addParameter(0, modelParam);
-		
+		adjustableParams.addParameter(1, modelInfoParam);
 	}
 	
 	/**
