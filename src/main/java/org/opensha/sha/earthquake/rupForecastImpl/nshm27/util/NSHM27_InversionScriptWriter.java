b6b1cbc8ebc99d6aabc7102ed887f9dec5003325
Kevin Milner <kmilner@usgs.gov>
2026-09-18 12:49:11 -0700

    utility class to build curve archives from result dirs

--- src/main/java/org/opensha/sha/earthquake/rupForecastImpl/nshm27/util/NSHM27_InversionScriptWriter.java
+++ src/main/java/org/opensha/sha/earthquake/rupForecastImpl/nshm27/util/NSHM27_InversionScriptWriter.java
@@ -47,6 +47,7 @@ public class NSHM27_InversionScriptWriter {
 		ops.addOption(null, "unique-seed", false, "Flag to use a unique seed from this calculation rather than the "
 				+ "deterministic seed based on the region and sample count/method.");
 		ops.addOption(null, "name-add", true, "Add arbitrary additional directory name strings");
+		ops.addOption(null, "date", true, "Date override");
 		
 		return ops;
 	}
@@ -58,41 +59,44 @@ public class NSHM27_InversionScriptWriter {
 			
 //			int samples = 5000;
 //			int samples = 10000;
-//			int samples = 20000;
+			int samples = 20000;
 //			int samples = 50000;
 			// 1 << N = 2^N
 //			int samples = 1 << 9; // 512
 //			int samples = 1 << 10; // 1024
 //			int samples = 1 << 11; // 2048
-			int samples = 1 << 12; // 4096
+//			int samples = 1 << 12; // 4096
 //			int samples = 1 << 13; // 8192
+//			int samples = 1 << 14; // 16384
 			args = new String[] {
 					"--region", NSHM27_SeismicityRegions.AMSAM.name(),
 //					"--region", NSHM27_SeismicityRegions.GNMI.name(),
 					
 					"--local-dir", "/home/kevin/OpenSHA/fss_inversions",
 					
-					"--hpc-site", HPCConfig.HPCSite.USC_CARC_FMPJ.name(),
-					"--remote-dir", "/project2/scec_608/kmilner/fss_inversions",
-					"--nodes", "36",
-//					"--job-time-hours", "48",
-					"--job-time-hours", "10",
-					
-//					"--hpc-site", HPCConfig.HPCSite.FRONTERA_FMPJ.name(),
-//					"--remote-dir", "/scratch2/00950/kevinm/fss_inversions",
-//					"--nodes", "40",
-////					"--job-time-hours", "20", // for 20k
-//					"--job-time-hours", "10", // for 8192
+//					"--hpc-site", HPCConfig.HPCSite.USC_CARC_FMPJ.name(),
+//					"--remote-dir", "/project2/scec_608/kmilner/fss_inversions",
+//					"--nodes", "36",
 ////					"--job-time-hours", "48",
-////					"--hazard-time-same-as-inversion",
+//					"--job-time-hours", "10",
 					
-//					"--sampling-method", SamplingMethod.MONTE_CARLO.name(),
+					"--hpc-site", HPCConfig.HPCSite.FRONTERA_FMPJ.name(),
+					"--remote-dir", "/scratch2/00950/kevinm/fss_inversions",
+					"--nodes", "40",
+					"--job-time-hours", "20", // for 20k
+//					"--job-time-hours", "10", // for 8192
+//					"--job-time-hours", "48",
+//					"--hazard-time-same-as-inversion",
+					
+//					"--date", "2026_09_10",
+					
+					"--sampling-method", SamplingMethod.MONTE_CARLO.name(),
 //					"--sampling-method", SamplingMethod.OWEN_SCRAMBLED_SOBOL.name(),
 //					"--sampling-method", SamplingMethod.LATIN_HYPERCUBE.name(),
-					"--sampling-method", SamplingMethod.PAIRWISE_OPTIMIZED_LATIN_HYPERCUBE.name(),
+//					"--sampling-method", SamplingMethod.PAIRWISE_OPTIMIZED_LATIN_HYPERCUBE.name(),
 					"--samples", samples+"",
 					"--unique-seed",
-					"--name-add", "7",
+					"--name-add", "20",
 					
 					"--hazard-grid-spacing", "0.2",
 					"--write-hazard-curves",
@@ -129,6 +133,7 @@ public class NSHM27_InversionScriptWriter {
 				.addNameToken(samplingMethod.getFilePrefix())
 				.addNameToken(deterministicSeed ? null : "unique_seed")
 				.addNameTokens(nameAdds)
+				.datePrefix(cmd.hasOption("date") ? cmd.getOptionValue("date") : null)
 				.build();
 
 		HPCConfig hpc = HPCConfig.builder(cmd)
